94a0a6a3d3f3bfb476088e1e066d0ab3c7ced77f
Austin Hilberg <40366950+austin-hilberg@users.noreply.github.com>
2018-08-15 12:36:37 -0500

    Add PassiveTrialSet table with PassiveTrial as a Part.

--- behavior.py
+++ behavior.py
@@ -53,9 +53,9 @@ class WheelMoveSet(dj.Imported):
         ---
         wheel_move_start_time:  float   # onset time of the detected wheel movement (seconds)
         wheel_move_end_time:    float   # offset time of the detected wheel movement (seconds)
-        -> WheelMoveType     
+        -> WheelMoveType
         """
-        
+
 
 @schema
 class SparseNoise(dj.Imported):
@@ -63,7 +63,7 @@ class SparseNoise(dj.Imported):
     -> acquisition.Session
     ---
     sparse_noise_x_pos:  longblob				# x coordiate on screen of sparse noise stimulus squares (WHAT UNIT?)
-    sparse_noise_y_pos:  longblob				# x coordiate on screen of sparse noise stimulus squares (WHAT UNIT?) 
+    sparse_noise_y_pos:  longblob				# x coordiate on screen of sparse noise stimulus squares (WHAT UNIT?)
     sparse_noise_times:  longblob				# times of those stimulus squares appeared in universal seconds
     """
 
@@ -121,14 +121,14 @@ class TrialSet(dj.Imported):
         trial_start_time:       float         # beginning of quiescent period time (seconds)
         trial_end_time:         float         # end of iti (seconds)
         trial_go_cue_time:      float         # Time of go cue in choiceworld (seconds)
-        trial_response_time:    float         # Time of "response" in choiceworld (seconds). This is when one of the three possible choices is registered in software, will not be the same as when the mouse's movement to generate that response begins. 
+        trial_response_time:    float         # Time of "response" in choiceworld (seconds). This is when one of the three possible choices is registered in software, will not be the same as when the mouse's movement to generate that response begins.
         trial_choice:           enum("CCW", "CW", "No Go")       # which choice was made in choiceworld
         trial_stim_on_time:     float         # Time of stimulus in choiceworld (seconds)
         trial_stim_position:    enum("Left", "Right")	 # position of the stimulus
         trial_stim_contrast:    float         # contrast of the stimulus
         trial_feedback_time:    float         # Time of feedback delivery (reward or not) in choiceworld
         trial_feedback_type:    tinyint       # whether feedback is positive or negative in choiceworld (-1 for negative, +1 for positive)
-        trial_rep_num:          int     	  # the repetition number of the trial, i.e. how many trials have been repeated on this side (counting from 1)        
+        trial_rep_num:          int     	  # the repetition number of the trial, i.e. how many trials have been repeated on this side (counting from 1)
         """
 
 @schema
@@ -138,21 +138,26 @@ class ExcludedTrial(dj.Imported):
     """
 
 @schema
-class PassiveTrial(dj.Imported):
-    # it is possible to be a part table of TrialSet if the repitition number could be inferred from the data,
-    # Are passive trials interleaved with real trials?
-    # all times are in absolute seconds, rather than relative to trial onset
+class PassiveTrialSet(dj.Imported):
     definition = """
     -> acquisition.Session
-    passive_trial_id:           int         # trial identifier
     ---
-    passive_trial_included:         boolean		             # suggesting whether this trial to include in analysis, chosen at experimenter discretion, e.g. by excluding the block of incorrect trials at the end of the session when the mouse has stopped
-    passive_trial_stim_on_time:     float	                 # Time of stimuli in choiceworld
-    passive_trial_stim_position:    enum("Left", "Right")	 # position of the stimulus
-    passive_trial_contrast:         float                    # contrast of the stimulus				    
-    passive_valve_click_time:       float 		             # Time of valve opening during passive trial presentation (seconds)
-    passive_beep_time:              float         		     # Time of the beep, equivilent to the go cue during the choice world task (seconds)
-    passive_white_noise_time:       float		             # Time of white noise bursts, equivilent to the negative feedback sound during the choice world task (seconds)
-    passive_noise_start_time:       float  	                 # passive noise trial's start(i.e. beginning of quiescent period) (seconds)
-    passive_noise_end_time:         float                    # passive noise trial's stop (i.e. end of iti) (seconds)
+    passive_trials_total_num : int
+    passive_trials_start_time : float
+    passive_trials_end_time : float
     """
+    class PassiveTrial(dj.Part):
+        definition = """
+        -> master
+        passive_trial_id:           int         # trial identifier
+        ---
+        passive_trial_included:         boolean		             # suggesting whether this trial to include in analysis, chosen at experimenter discretion, e.g. by excluding the block of incorrect trials at the end of the session when the mouse has stopped
+        passive_trial_stim_on_time:     float	                 # Time of stimuli in choiceworld
+        passive_trial_stim_position:    enum("Left", "Right")	 # position of the stimulus
+        passive_trial_contrast:         float                    # contrast of the stimulus
+        passive_valve_click_time:       float 		             # Time of valve opening during passive trial presentation (seconds)
+        passive_beep_time:              float         		     # Time of the beep, equivilent to the go cue during the choice world task (seconds)
+        passive_white_noise_time:       float		             # Time of white noise bursts, equivilent to the negative feedback sound during the choice world task (seconds)
+        passive_noise_start_time:       float  	                 # passive noise trial's start(i.e. beginning of quiescent period) (seconds)
+        passive_noise_end_time:         float                    # passive noise trial's stop (i.e. end of iti) (seconds)
+        """
