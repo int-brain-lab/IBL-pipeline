68c9327b1f04a66e59cc21c6387502d6070a0818
Shan Shen <shenshanpku@gmail.com>
2018-09-13 18:35:23 -0500

    Ingest into the AlyxRaw table.

--- ibl/ingest/__init__.py
+++ ibl/ingest/__init__.py
@@ -52,3 +52,43 @@ While this should not happen in the current architecture, following the pattern
 outlined here should prevent it in general and so is a good 'safe practice' to
 use for the ingest modules.
 '''
+import logging
+import datajoint as dj
+
+
+log = logging.getLogger(__name__)
+
+
+class InsertBuffer(object):
+    '''
+    InsertBuffer: a utility class to help managed chunked inserts
+    Currently requires records do not have prerequisites.
+    '''
+    def __init__(self, rel):
+        self._rel = rel
+        self._queue = []
+
+    def insert1(self, r):
+        self._queue.append(r)
+
+    def insert(self, recs):
+        self._queue += recs
+
+    def flush(self, replace=False, skip_duplicates=False,
+              ignore_extra_fields=False, ignore_errors=False, chunksz=1):
+        '''
+        flush the buffer
+        XXX: use kwargs?
+        XXX: ignore_extra_fields na, requires .insert() support
+        '''
+        qlen = len(self._queue)
+        if qlen > 0 and qlen % chunksz == 0:
+            try:
+                self._rel.insert(self._queue, skip_duplicates=skip_duplicates,
+                                 ignore_extra_fields=ignore_extra_fields,
+                                 ignore_errors=ignore_errors)
+                self._queue.clear()
+                return True
+            except dj.DataJointError as e:
+                log.error('error in flush: {}'.format(e))
+                raise
