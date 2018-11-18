a595237172b0eb49f8fa730d9604c46937608921
Edgar Y. Walker <edgar.walker@gmail.com>
2018-11-18 11:46:41 -0600

    Update scripts

--- scripts/ingest_behavior.py
+++ scripts/ingest_behavior.py
@@ -4,8 +4,8 @@ This script ingest behavioral data into tables in the ibl_behavior schema
 
 from ibl_pipeline import subject, acquisition, data, behavior
 
-
+behavior.CompleteWheelSession.populate()
 behavior.Wheel.populate()
 # behavior.WheelMoveSet.populate()
-behavior.CompleteSession.populate()
+behavior.CompleteTrialSession.populate()
 behavior.TrialSet.populate()
