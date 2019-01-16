f578ed387aed0aeb93027af50932d69fddc72d15
Shan Shen <shenshanpku@gmail.com>
2019-01-16 10:29:21 -0800

    Move behavioral_snapshots into prelim_analyses.

--- scripts/ingest_all.sh
+++ scripts/ingest_all.sh
@@ -3,6 +3,6 @@ python ingest_alyx_shadow.py
 python ingest_alyx_shadow_membership.py
 python ingest_alyx_real.py
 python ingest_behavior.py
-cd ../behavioral_snapshots
+cd ../prelim_analyses/behavioral_snapshots
 python behavioral_snapshot.py
 python behavioral_overview_perlab.py
