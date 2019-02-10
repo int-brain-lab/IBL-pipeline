cb304a302fab397b1fd00a7be8263e0acf8d34d0
Shan Shen <shenshanpku@gmail.com>
2019-02-09 18:33:45 -0800

    Change the order

--- scripts/ingest_all.sh
+++ scripts/ingest_all.sh
@@ -1,5 +1,5 @@
-python ingest_alyx_raw.py "$@"
 python delete_tables_for_updates.py
+python ingest_alyx_raw.py "$@"
 python ingest_alyx_shadow.py
 python ingest_alyx_shadow_membership.py
 python ingest_alyx_real.py
