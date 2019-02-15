d793c2bdeb94d42e2a9d3a65d4a8e55c520cf6e4
Shan Shen <shenshanpku@gmail.com>
2019-02-15 10:28:52 -0500

    Also populate session with stimOn_times missing

--- ibl_pipeline/behavior.py
+++ ibl_pipeline/behavior.py
@@ -388,7 +388,7 @@ class TrialSet(dj.Imported):
     """
 
     # Knowledge based hack to be formalized better later
-    key_source = CompleteTrialSession & 'trial_session_complete=1'
+    key_source = CompleteTrialSession
 
     def make(self, key):
         trial_key = key.copy()
