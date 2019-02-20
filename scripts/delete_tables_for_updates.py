6a3ccbb2c21bb18ee65229ed1a9c0e81f1a579aa
Shan Shen <shenshanpku@gmail.com>
2019-02-19 21:15:16 -0800

    Change back to overall deletion. Transaction problem to be fixed.

--- scripts/delete_tables_for_updates.py
+++ scripts/delete_tables_for_updates.py
@@ -6,13 +6,13 @@ from ibl_pipeline import action, data
 dj.config['safemode'] = False
 
 # delete alyxraw except for datasets and file records
-(alyxraw.AlyxRaw & 'model not in ("data.dataset", "data.filerecord")').delete()
+#(alyxraw.AlyxRaw & 'model not in ("data.dataset", "data.filerecord")').delete()
 
 # delete alyxraw for data.filerecord if exists = 0
-file_records = alyxraw.AlyxRaw & 'model = "data.filerecord"'
-file_record_fields = alyxraw.AlyxRaw.Field & file_records & 'fname = "exists"' & 'fvalue = "False"'
-keys = (alyxraw.AlyxRaw & file_record_fields).fetch('KEY')
-(alyxraw.AlyxRaw & keys).delete()
+# file_record_fields = alyxraw.AlyxRaw.Field & 'fname = "exists"' & 'fvalue = "False"'
+# keys = (alyxraw.AlyxRaw & file_record_fields).fetch('KEY')
+# (alyxraw.AlyxRaw & keys).delete()
+alyxraw.AlyxRaw.delete()
 
 # delete some shadow tables
 action_shadow.WaterRestrictionProcedure.delete()
