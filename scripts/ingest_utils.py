d8d200b2875d8cbc54c06fc18d6c674016591ac0
Shan Shen <shenshanpku@gmail.com>
2019-02-23 07:44:28 -0800

    Insert entries one by one if failed on table insertion.

--- scripts/ingest_utils.py
+++ scripts/ingest_utils.py
@@ -6,8 +6,15 @@ Utility functions for ingestion
 def copy_table(target_schema, src_schema, table_name, fresh=False):
     target_table = getattr(target_schema, table_name)
     src_table = getattr(src_schema, table_name)
-
+   
     if fresh:
-        target_table.insert(src_table, skip_duplicates=True)
+        target_table.insert(src_table)
     else:
-        target_table.insert(src_table - target_table.proj(), skip_duplicates=True)
+        try:
+            target_table.insert(src_table - target_table.proj())
+        except:
+            for t in (src_table - target_table.proj()).fetch(as_dict=True):
+                try:
+                    target_table.insert1(t)
+                except:
+                    print("Error when inserting {}".format(t))
