27752e4a37484e8cda30fd811ae3102ce968b1f8
Shan Shen <shenshanpku@gmail.com>
2019-02-28 16:25:35 -0800

    Remove reaction time from the PsychResults table.

--- ibl_pipeline/analyses/behavior.py
+++ ibl_pipeline/analyses/behavior.py
@@ -1,6 +1,6 @@
 import datajoint as dj
 from .. import subject, action, acquisition, behavior
-from . import psychofit
+from . import psychofit as psy
 import numpy as np
 
 
@@ -25,11 +25,18 @@ class Contrasts(dj.Computed):
     """
 
     def make(self, key):
-        print(key)
+
         key_con = key.copy()
         trials = behavior.TrialSet.Trial & key
-        contrasts_left = np.unique(trials.fetch('trial_stim_contrast_left'))[1:] # discard contrast 0
-        contrasts_right = np.unique(trials.fetch('trial_stim_contrast_right'))[1:] # discard contrast 0
+        contrasts_left = np.unique(trials.fetch('trial_stim_contrast_left'))
+        contrasts_right = np.unique(trials.fetch('trial_stim_contrast_right'))
+        # discard contrast 0
+        if contrasts_left[0] == 0:
+            contrasts_left = contrasts_left[1:]
+
+        if contrasts_right[0] == 0:
+            contrasts_right = contrasts_right[1:]
+        
         trials_no_contrast = trials & 'trial_stim_contrast_right=0' & 'trial_stim_contrast_left=0'
 
         key_con['has_left'] = False
@@ -73,10 +80,9 @@ class Contrasts(dj.Computed):
 @schema
 class PsychResults(dj.Computed):
     definition = """
-    -> behavior.TrialSet
+    -> Contrasts
     ---
     performance:        float   # percentage correct in this session
-    contrasts:          blob    # contrasts used in this session
     prob_choose_right:  blob    # probability of choosing right, same size as contrasts
     threshold:          float
     bias:               float
@@ -90,50 +96,55 @@ class PsychResults(dj.Computed):
         
         key_psy = key.copy()
 
-        n_trials, n_correct_trials = (behavior.TrialSet & key).fetch1('n_trials', 'n_correct_trials')
+        n_trials, n_correct_trials = (behavior.TrialSet & key).fetch1(
+            'n_trials', 'n_correct_trials')
         key_psy['performance'] = n_correct_trials/n_trials
 
         trials = behavior.TrialSet.Trial & key
-    
-        contrasts_left = np.unique(trials.fetch('trial_stim_contrast_left'))[1:] # discard contrast 0
-        contrasts_right = np.unique(trials.fetch('trial_stim_contrast_right'))[1:] # discard contrast 0
-        trials_no_contrast = trials & 'trial_stim_contrast_right=0' & 'trial_stim_contrast_left=0'
-        
-        print(contrasts_left)
-        print(contrasts_right)
-        n_right_trials_stim_left = [len(trials & 'trial_stim_contrast_left={}'.format(contrast) & 'trial_response_choice="CCW"') \
-                                    for contrast in contrasts_left]
-        n_trials_stim_left = [len(trials & 'trial_stim_contrast_left={}'.format(contrast)) \
-                                    for contrast in contrasts_left]
-        n_right_trials_stim_right = [len(trials & 'trial_stim_contrast_right={}'.format(contrast) & 'trial_response_choice="CCW"') \
-                                    for contrast in contrasts_right]
-        n_trials_stim_right = [len(trials & 'trial_stim_contrast_right={}'.format(contrast)) \
-                                    for contrast in contrasts_right]
 
-        p_right_stim_left = np.divide(n_right_trials_stim_left, n_trials_stim_left)
-        print(n_trials_stim_left)
-        p_right_stim_right = np.divide(n_right_trials_stim_right, n_trials_stim_right)
+        contrasts, has_left, has_right, has_no_contrast, n_trials_stim = (Contrasts & key).fetch1(
+            'contrasts', 'has_left', 'has_right', 'has_no_contrast', 'n_trials_stim')
     
-        trials_no_contrast = trials & 'trial_stim_contrast_right=0' & 'trial_stim_contrast_left=0'
-        if trials_no_contrast:
-            key_psy['contrasts'] = np.hstack([np.negative(contrasts_left[::-1]), 0,  contrasts_right])*100
-            
-            p_right_no_contrast = len(trials_no_contrast & 'trial_response_choice="CCW"')/len(trials_no_contrast)            
-            key_psy['prob_choose_right'] = np.hstack([p_right_stim_left[::-1], p_right_no_contrast, p_right_stim_right])
-            n_total_trials = np.hstack([n_trials_stim_left, len(trials_no_contrast), n_trials_stim_right])
-            
+        if has_left:
+            contrasts_left, n_trials_stim_left = (Contrasts & key).fetch1(
+                'contrasts_left', 'n_trials_stim_left')
+            n_right_trials_stim_left = [len(trials & 'trial_stim_contrast_left={}'.format(contrast) & \
+                                            'trial_response_choice="CCW"') \
+                                        for contrast in contrasts_left]
+            p_right_stim_left = np.divide(n_right_trials_stim_left, n_trials_stim_left)
         else:
-            key_psy['contrasts'] = np.hstack([np.negative(contrasts_left[::-1]),  contrasts_right])*100
-            key_psy['prob_choose_right'] = np.hstack([(p_right_stim_left[::-1]), p_right_stim_right])
-            n_total_trials = np.hstack([n_trials_stim_left, n_trials_stim_right])
-           
+            p_right_stim_left = []
 
-        pars, L = psy.mle_fit_psycho(np.vstack([key_psy['contrasts'], n_total_trials, key_psy['prob_choose_right']]), \
+        
+        if has_right:
+            contrasts_right, n_trials_stim_right = (Contrasts & key).fetch1(
+                'contrasts_right', 'n_trials_stim_right')
+            n_right_trials_stim_right = [len(trials & 'trial_stim_contrast_right={}'.format(contrast) & \
+                                            'trial_response_choice="CCW"') \
+                                        for contrast in contrasts_right]
+            p_right_stim_right = np.divide(n_right_trials_stim_right, n_trials_stim_right)
+        else:
+            p_right_stim_right = []
+        
+        if has_no_contrast:
+            n_trials_stim_no_contrast = (Contrasts & key).fetch1(
+                'n_trials_stim_no_contrast')
+            trials_no_contrast = trials & 'trial_stim_contrast_right=0' & 'trial_stim_contrast_left=0'
+            p_right_no_contrast = len(trials_no_contrast & 'trial_response_choice="CCW"')/n_trials_stim_no_contrast            
+        else:
+            p_right_no_contrast = []
+        
+        prob_choose_right = np.hstack([p_right_stim_left, p_right_no_contrast, p_right_stim_right])
+
+        print(contrasts)
+        
+        pars, L = psy.mle_fit_psycho(np.vstack([contrasts, n_trials_stim, prob_choose_right]), \
                 P_model='erf_psycho_2gammas', \
-                parstart=np.array([key_psy['contrasts'].mean(), 20., 0.05, 0.05]), \
-                parmin=np.array([key_psy['contrasts'].mean(), 0., 0., 0.]), \
-                parmax=np.array([key_psy['contrasts'].mean(), 100., 1, 1]))
+                parstart=np.array([contrasts.mean(), 20., 0.05, 0.05]), \
+                parmin=np.array([contrasts.mean(), 0., 0., 0.]), \
+                parmax=np.array([contrasts.mean(), 100., 1, 1]))
 
+        key_psy['prob_choose_right'] = prob_choose_right
         key_psy['bias'] = pars[0]
         key_psy['threshold'] = pars[1] 
         key_psy['lapse_low'] = pars[2]
