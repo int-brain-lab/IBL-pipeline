a7e3e9caa2474f3528d0ffb28148b56e041328d8
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-03-29 15:16:08 -0700

    fix bug for deleting real tables.

--- scripts/delete_real_tables_for_updates.py
+++ scripts/delete_real_tables_for_updates.py
@@ -7,11 +7,11 @@ from ibl_pipeline.ingest import action as action_ingest
 dj.config['safemode'] = False
 
 # delete some real tables when the shadow tables are available
-if len(action_ingest.Weighing):
+if len(action_ingest.Weighing()):
     action.Weighing.delete()
 
-if len(action_ingest.WaterAdiminitration):
+if len(action_ingest.WaterAdiminitration()):
     action.WaterAdministration.delete()
 
-if len(action_ingest.WaterRestriction):
+if len(action_ingest.WaterRestriction()):
     action.WaterRestriction.delete()
