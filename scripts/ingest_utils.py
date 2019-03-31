f2194fb4f16b7dac1218a466f4e15695490d3db0
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-03-31 15:48:40 -0700

    import all tables for copying tables.

--- scripts/ingest_utils.py
+++ scripts/ingest_utils.py
@@ -2,16 +2,25 @@
 Utility functions for ingestion
 '''
 import traceback
+import datajoint as dj
+from ibl_pipeline.ingest import reference as reference_ingest
+from ibl_pipeline.ingest import subject as subject_ingest
+from ibl_pipeline.ingest import action as action_ingest
+from ibl_pipeline.ingest import acquisition as acquisition_ingest
+from ibl_pipeline.ingest import data as data_ingest
+from ibl_pipeline import reference, subject, action, acquisition, data
+
 
 def copy_table(target_schema, src_schema, table_name, fresh=False):
     target_table = getattr(target_schema, table_name)
     src_table = getattr(src_schema, table_name)
-   
+
     if fresh:
         target_table.insert(src_table)
     else:
         try:
-            target_table.insert(src_table - target_table.proj(), skip_duplicates=True)
+            target_table.insert(src_table - target_table.proj(),
+                                skip_duplicates=True)
         except:
             for t in (src_table - target_table.proj()).fetch(as_dict=True):
                 try:
