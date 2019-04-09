ab47ddba28dd9d5c077de05086466e3782553a4f
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-04-08 23:35:26 -0700

    Only ingest trials table for dev.

---
+++ scripts/ingest_all_dev.sh
@@ -0,0 +1,7 @@
+python delete_shadow_tables_for_updates.py
+python ingest_alyx_raw.py "$@"
+python ingest_alyx_shadow.py
+python ingest_alyx_shadow_membership.py
+python delete_real_tables_for_updates.py
+python ingest_alyx_real.py
+python behavior_ingest_analyses.py
