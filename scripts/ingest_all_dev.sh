053c93fae44c71c1d80029fe97e3a92d618ea292
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-04-08 23:40:22 -0700

    Add timer to script

--- scripts/ingest_all_dev.sh
+++ scripts/ingest_all_dev.sh
@@ -1,3 +1,4 @@
+start=`date +%s`
 python delete_shadow_tables_for_updates.py
 python ingest_alyx_raw.py "$@"
 python ingest_alyx_shadow.py
@@ -5,3 +6,5 @@ python ingest_alyx_shadow_membership.py
 python delete_real_tables_for_updates.py
 python ingest_alyx_real.py
 python behavior_ingest_analyses.py
+end=`date +%s`
+runtime=$((end-start))
