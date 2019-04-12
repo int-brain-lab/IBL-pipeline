38ee1990aa019849e50eb14a497f9cb73013bbdb
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-04-11 22:52:28 -0700

    put output in the log file.

--- scripts/ingest_all_dev.sh
+++ scripts/ingest_all_dev.sh
@@ -1,3 +1,4 @@
+exec &> log_dev.txt
 start=`date +%s`
 python delete_shadow_tables_for_updates.py
 python ingest_alyx_raw.py "$@"
@@ -5,6 +6,11 @@ python ingest_alyx_shadow.py
 python ingest_alyx_shadow_membership.py
 python delete_real_tables_for_updates.py
 python ingest_alyx_real.py
-python behavior_ingest_analyses.py
+python ingest_behavior.py
+python analyses_behavior.py
+python create_summary_csv.py
+cd ../prelim_analyses/behavioral_snapshots
+python behavioral_snapshot.py
+python behavioral_overview_perlab.py
 end=`date +%s`
 runtime=$((end-start))
