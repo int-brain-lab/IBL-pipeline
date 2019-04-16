4c27317f7101d8a167837096968524a414be9434
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-04-16 06:56:25 -0700

    Install plotly in docker

--- scripts/analyses_behavior.py
+++ scripts/analyses_behavior.py
@@ -1,6 +1,6 @@
 from ibl_pipeline.analyses import behavior
 
-behavior.PsychResults.populate(diplay_progress=True, suppress_errors=True)
+behavior.PsychResults.populate(display_progress=True, suppress_errors=True)
 behavior.ReactionTime.populate(display_progress=True, suppress_errors=True)
 behavior.SessionTrainingStatus.populate(diplay_progress=True,
                                         suppress_errors=True)
