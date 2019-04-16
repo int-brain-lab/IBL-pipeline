07345adce5e0c1268f20d56d98ff2c772e380f25
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-04-16 07:09:54 -0700

    Fix typo.

--- scripts/analyses_behavior.py
+++ scripts/analyses_behavior.py
@@ -2,7 +2,7 @@ from ibl_pipeline.analyses import behavior
 
 behavior.PsychResults.populate(display_progress=True, suppress_errors=True)
 behavior.ReactionTime.populate(display_progress=True, suppress_errors=True)
-behavior.SessionTrainingStatus.populate(diplay_progress=True,
+behavior.SessionTrainingStatus.populate(display_progress=True,
                                         suppress_errors=True)
 behavior.BehavioralSummaryByDate.populate(display_prgress=True,
                                           suppress_errors=True)
