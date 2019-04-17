c8100cd25c14d882dc637337fc448cefc9c23bcb
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-04-17 08:37:24 -0700

    Fix typo.

--- scripts/analyses_behavior.py
+++ scripts/analyses_behavior.py
@@ -4,5 +4,5 @@ behavior.PsychResults.populate(display_progress=True, suppress_errors=True)
 behavior.ReactionTime.populate(display_progress=True, suppress_errors=True)
 behavior.SessionTrainingStatus.populate(display_progress=True,
                                         suppress_errors=True)
-behavior.BehavioralSummaryByDate.populate(display_prgress=True,
+behavior.BehavioralSummaryByDate.populate(display_progress=True,
                                           suppress_errors=True)
