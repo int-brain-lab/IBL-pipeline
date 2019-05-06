2b5b38d9db70a0ea3052ee030f8f0524bf942013
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-05-06 13:25:48 -0700

    change LatestDate to a manual table and perform manual insertion.

--- ibl_pipeline/plotting/behavior.py
+++ ibl_pipeline/plotting/behavior.py
@@ -279,7 +279,7 @@ class DateReactionTimeTrialNumber(dj.Computed):
 
 
 @schema
-class LatestDate(dj.Computed):
+class LatestDate(dj.Manual):
     # compute the last date of any event for individual subjects
     definition = """
     -> subject.Subject
@@ -287,44 +287,6 @@ class LatestDate(dj.Computed):
     ---
     latest_date: date
     """
-
-    def make(self, key):
-
-        latest_behavior = (subject.Subject & key).aggr(
-            behavior.BehavioralSummaryByDate,
-            last_behavior_date='MAX(session_date)')
-        latest_weight = (subject.Subject & key).aggr(
-            action.Weighing,
-            last_weighing_date='DATE(MAX(weighing_time))')
-        latest_water = (subject.Subject & key).aggr(
-            action.WaterAdministration,
-            last_water_date='DATE(MAX(administration_time))')
-
-        water_weight = (latest_water * latest_weight).proj(
-            latest_water_weight='GREATEST(last_water_date, last_weighing_date)'
-        )
-
-        if not(latest_behavior or water_weight):
-            return
-        elif latest_behavior and water_weight:
-            last_behavior_date = latest_behavior.fetch1(
-                'last_behavior_date'
-            )
-            last_water_weight_date = water_weight.fetch1(
-                'latest_water_weight'
-            )
-            latest_date = max([last_behavior_date, last_water_weight_date])
-        elif latest_behavior:
-            latest_date = latest_behavior.fetch1(
-                'last_behavior_date'
-            )
-        elif water_weight:
-            latest_date = water_weight.fetch1(
-                'latest_water_weight'
-            )
-
-        key['latest_date'] = latest_date
-        self.insert1(key)
 
 
 # @schema
---
+++ scripts/compute_latest_date.py
@@ -0,0 +1,48 @@
+'''
+This script computes the latest event date happen to individual subject
+and manually insert into the table behavior_plotting.LatestDate
+'''
+
+import datajoint as dj
+from ibl_pipeline import reference, subject, action, acquisition, data, behavior
+from ibl_pipeline.analyses import behavior as behavior_analyses
+import numpy as np
+from datetime import datetime
+
+
+for key in subject.Subject():
+    latest_behavior = (subject.Subject & key).aggr(
+        behavior.BehavioralSummaryByDate,
+        last_behavior_date='MAX(session_date)')
+    latest_weight = (subject.Subject & key).aggr(
+        action.Weighing,
+        last_weighing_date='DATE(MAX(weighing_time))')
+    latest_water = (subject.Subject & key).aggr(
+        action.WaterAdministration,
+        last_water_date='DATE(MAX(administration_time))')
+
+    water_weight = (latest_water * latest_weight).proj(
+        latest_water_weight='GREATEST(last_water_date, last_weighing_date)'
+    )
+
+    if not(latest_behavior or water_weight):
+        return
+    elif latest_behavior and water_weight:
+        last_behavior_date = latest_behavior.fetch1(
+            'last_behavior_date'
+        )
+        last_water_weight_date = water_weight.fetch1(
+            'latest_water_weight'
+        )
+        latest_date = max([last_behavior_date, last_water_weight_date])
+    elif latest_behavior:
+        latest_date = latest_behavior.fetch1(
+            'last_behavior_date'
+        )
+    elif water_weight:
+        latest_date = water_weight.fetch1(
+            'latest_water_weight'
+        )
+
+    key['latest_date'] = latest_date
+    self.insert1(key)
