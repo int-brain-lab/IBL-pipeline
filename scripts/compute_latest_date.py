61027ca9d81d3fc89aed2a958adc7ad7b9f0169e
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-05-06 14:07:39 -0700

    change LatestDate to a manual table and perform manual insertion.

--- scripts/compute_latest_date.py
+++ scripts/compute_latest_date.py
@@ -12,37 +12,43 @@ from datetime import datetime
 
 
 for key in subject.Subject():
-    latest_behavior = (subject.Subject & key).aggr(
-        behavior_analyses.BehavioralSummaryByDate,
-        last_behavior_date='MAX(session_date)')
-    latest_weight = (subject.Subject & key).aggr(
-        action.Weighing,
-        last_weighing_date='DATE(MAX(weighing_time))')
-    latest_water = (subject.Subject & key).aggr(
-        action.WaterAdministration,
-        last_water_date='DATE(MAX(administration_time))')
+    behavior_summary = behavior_analyses.BehavioralSummaryByDate & key
+    water_weight = action.Weighing * action.WaterAdministration & key
+    if behavior_summary:
+        latest_behavior = (subject.Subject & key).aggr(
+            behavior_analyses.BehavioralSummaryByDate,
+            last_behavior_date='MAX(session_date)')
 
-    water_weight = (latest_water * latest_weight).proj(
-        latest_water_weight='GREATEST(last_water_date, last_weighing_date)'
-    )
+    if water_weight:
+        latest_weight = (subject.Subject & key).aggr(
+            action.Weighing,
+            last_weighing_date='DATE(MAX(weighing_time))')
+        latest_water = (subject.Subject & key).aggr(
+            action.WaterAdministration,
+            last_water_date='DATE(MAX(administration_time))')
 
-    if not(latest_behavior or water_weight):
+        latest_water_weight = (latest_water * latest_weight).proj(
+            last_water_weight_date='GREATEST(last_water_date, \
+                                             last_weighing_date)'
+        )
+
+    if not(behavior_summary or water_weight):
         continue
-    elif latest_behavior and water_weight:
+    elif behavior_summary and water_weight:
         last_behavior_date = latest_behavior.fetch1(
             'last_behavior_date'
         )
         last_water_weight_date = water_weight.fetch1(
-            'latest_water_weight'
+            'last_water_weight_date'
         )
         latest_date = max([last_behavior_date, last_water_weight_date])
-    elif latest_behavior:
+    elif behavior_summary:
         latest_date = latest_behavior.fetch1(
             'last_behavior_date'
         )
     elif water_weight:
         latest_date = water_weight.fetch1(
-            'latest_water_weight'
+            'last_water_weight_date'
         )
 
     key['latest_date'] = latest_date
