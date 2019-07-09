4ed0703aedf77b888d1b85e0bbfc69a9f79083e3
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-07-08 19:08:02 -0700

    Fix bug

--- scripts/insert_update_subject_last_date.py
+++ scripts/insert_update_subject_last_date.py
@@ -10,9 +10,10 @@ subjects = subject.Subject.aggr(
     behavior.CumulativeSummary,
     latest_date='MAX(latest_date)')
 
-for subj in subjects.fetch():
+for subj in subjects.fetch('KEY'):
     current_subj = behavior.SubjectLatestDate & subj
+    new_date = (subjects & subj).fetch1('latest_date')
     if len(current_subj):
-        current_subj._update('latest_date', subj['latest_date'])
+        current_subj._update('latest_date', new_date)
     else:
-        behavior.SubjectLatestDate.insert1(subj, skip_duplicates=True)
+        behavior.SubjectLatestDate.insert1(subj)
