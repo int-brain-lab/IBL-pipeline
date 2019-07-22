39079511d72587c321075d9a7d7a1b1323a9fdaf
Shan Shen <shanshen@vpn10-client-128-112-67-239.princeton.edu>
2019-07-22 14:16:25 -0700

    Add Settings in daily populate

--- scripts/ingest_behavior.py
+++ scripts/ingest_behavior.py
@@ -4,8 +4,13 @@ This script ingest behavioral data into tables in the ibl_behavior schema
 
 from ibl_pipeline import subject, acquisition, data, behavior
 
-behavior.CompleteWheelMoveSession.populate(suppress_errors=True)
-behavior.WheelMoveSet.populate(display_progress=True, suppress_errors=True)
-behavior.CompleteTrialSession.populate(suppress_errors=True)
-behavior.TrialSet.populate(display_progress=True, suppress_errors=True)
-behavior.AmbientSensorData.populate(display_progress=True, suppress_errors=True)
+kargs = dict(
+    suppress_errors=True, display_progress=True
+)
+
+behavior.CompleteWheelMoveSession.populate(**kargs)
+behavior.WheelMoveSet.populate(**kargs)
+behavior.CompleteTrialSession.populate(**kargs)
+behavior.TrialSet.populate(**kargs)
+behavior.AmbientSensorData.populate(**kargs)
+behavior.Settings.populate(**kargs)
