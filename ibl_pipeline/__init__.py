97f25b7e172dd187af5bdfbd0cdd8107fda744f4
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-07-23 11:19:16 -0700

    Hide the credentials.

--- ibl_pipeline/__init__.py
+++ ibl_pipeline/__init__.py
@@ -9,17 +9,17 @@ dj.config['stores'] = {
     'ephys': dict(
         protocol='s3',
         endpoint='s3.amazonaws.com',
-        access_key='AKIAX2NIY5IRJVKSPHVW',
-        secret_key='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',
+        access_key=os.environ.get('S3_ACCESS'),
+        secret_key=os.environ.get('S3_SECRET'),
         bucket='ibl-dj-external',
-        location='/user_shan/ephys'
+        location='/ephys'
     ),
     'plotting': dict(
         protocol='s3',
         endpoint='s3.amazonaws.com',
-        access_key='AKIAX2NIY5IRJVKSPHVW',
-        secret_key='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',
+        access_key=os.environ.get('S3_ACCESS'),
+        secret_key=os.environ.get('S3_SECRET'),
         bucket='ibl-dj-external',
-        location='/user_shan/plotting'
+        location='/plotting'
     )
 }
