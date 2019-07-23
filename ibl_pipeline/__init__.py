c37b028a14ccf9e9db3a88901edeb1331f5ba6d5
Shan Shen <shanshen@vpn10-client-128-112-67-239.princeton.edu>
2019-07-22 18:25:17 -0700

    change access key

--- ibl_pipeline/__init__.py
+++ ibl_pipeline/__init__.py
@@ -8,16 +8,16 @@ if os.environ.get('MODE') == 'test':
 dj.config['stores'] = {
     'ephys': dict(
         protocol='s3',
-        endpoint='https://s3.amazonaws.com',
-        access_key='ibl_external',
+        endpoint='s3.amazonaws.com',
+        access_key='AKIAX2NIY5IRJVKSPHVW',
         secret_key='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',
         bucket='ibl-dj-external',
         location='ephys'
     ),
     'plotting': dict(
         protocol='s3',
-        endpoint='https://s3.amazonaws.com',
-        access_key='ibl_external',
+        endpoint='s3.amazonaws.com',
+        access_key='AKIAX2NIY5IRJVKSPHVW',
         secret_key='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',
         bucket='ibl-dj-external',
         location='plotting'
