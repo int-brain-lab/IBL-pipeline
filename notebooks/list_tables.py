d1646e3c96216cdac0e69a444eb5a30bc3e2c0b4
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-08-30 16:24:21 -0700

    Reorg old tables.

--- notebooks/list_tables.py
+++
@@ -1,8 +0,0 @@
-from datajoint.user_tables import UserTable
-import inspect
-
-def list_tables(schema):
-    for k in dir(schema):
-        t = getattr(schema, k)
-        if inspect.isclass(t) and issubclass(t, UserTable):
-            print(k)
