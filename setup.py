4411abbe56b8d2b9b8f05cd8527d2b309183e210
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-09-01 19:55:12 -0400

    Add requirements to IBL-pipeline set up.

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='ibl_pipeline',
-    version='0.1.1',
+    version='0.1.2',
     description='Datajoint schemas for IBL',
     author='Vathes',
     author_email='support@vathes.com',
     packages=find_packages(exclude=[]),
-    install_requires=['datajoint>=0.12.dev5'],
+    install_requires=['datajoint>=0.12.dev5', 'scikits.bootstrap>=1.0.0', 'statsmodels>=0.10.1', 'plotly>=4.1.0', 'globus_sdk>=1.8.0'],
     scripts=['scripts/ibl-shell.py'],
 )
