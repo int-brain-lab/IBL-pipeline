2060bb3f0dcba0f7aeb495dd69690cafb3f8eb7a
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-09-01 22:36:07 -0400

    Remove version number for bootstrap in setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     author='Vathes',
     author_email='support@vathes.com',
     packages=find_packages(exclude=[]),
-    install_requires=['datajoint>=0.12.dev5', 'scikits.bootstrap>=1.0.0', 'statsmodels>=0.10.1', 'plotly>=4.1.0', 'globus_sdk>=1.8.0'],
+    install_requires=['datajoint>=0.12.dev5', 'scikits.bootstrap', 'statsmodels>=0.10.1', 'plotly>=4.1.0'],
     scripts=['scripts/ibl-shell.py'],
 )
