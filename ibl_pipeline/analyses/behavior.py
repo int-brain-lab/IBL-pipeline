35e30aa65a80efde9bec04399bf58af0b2005559
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-09-12 14:28:28 -0700

    New behavioral criteria

--- ibl_pipeline/analyses/behavior.py
+++ ibl_pipeline/analyses/behavior.py
@@ -567,8 +567,6 @@ class SessionTrainingStatusNew(dj.Computed):
 
     def make(self, key):
 
-        print(key)
-
         subject_key = key.copy()
         subject_key.pop('session_start_time')
 
