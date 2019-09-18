1402d4a57c3bba3b23b8733b23cd321ccf55f547
shenshan <shenshanpku@gmail.com>
2019-09-18 12:05:39 -0700

    Public database

--- scripts/public/ingest_alyx_shadow.py
+++ scripts/public/ingest_alyx_shadow.py
@@ -8,70 +8,79 @@ from ibl_pipeline.ingest import alyxraw, reference, subject, action, acquisition
 from ibl_pipeline import public
 import utils
 
+kargs = dict(suppress_errors=True, display_progress=True)
+
 # reference tables
-reference.Lab.populate(suppress_errors=True)
-reference.LabMember.populate(suppress_errors=True)
-reference.LabMembership.populate(suppress_errors=True)
-reference.LabLocation.populate(suppress_errors=True)
-reference.Project.populate(suppress_errors=True)
+print('-------- Populating reference shadow tables ------------')
+reference.Lab.populate(**kargs)
+reference.LabMembership.populate(**kargs)
+reference.LabLocation.populate(**kargs)
+reference.Project.populate(**kargs)
 
 # subject tables
-subject.Species.populate(suppress_errors=True)
-subject.Source.populate(suppress_errors=True)
-subject.Strain.populate(suppress_errors=True)
-subject.Sequence.populate(suppress_errors=True)
-subject.Allele.populate(suppress_errors=True)
-subject.Line.populate(suppress_errors=True)
+print('-------- Populating subject shadow tables ------------')
+subject.Source.populate(**kargs)
+subject.Strain.populate(**kargs)
+subject.Sequence.populate(**kargs)
+subject.Allele.populate(**kargs)
+subject.Line.populate(**kargs)
 
 # select specific subjects for the public website
 subject_uuids = public.PublicSubjectUuid.fetch('subject_uuid')
 subj_res = utils.get_uuids('subjects.subject', 'subject_uuid', subject_uuids)
-subject.Subject.populate(subj_res, suppress_errors=True)
-# subject.BreedingPair.populate(suppress_errors=True)
-# subject.Litter.populate(suppress_errors=True)
-# subject.LitterSubject.populate(suppress_errors=True)
-subject.SubjectProject.populate(subj_res, suppress_errors=True)
-subject.SubjectUser.populate(subj_res, suppress_errors=True)
-subject.SubjectLab.populate(subj_res, suppress_errors=True)
-# subject.Caging.populate(suppress_errors=True)
-# subject.UserHistory.populate(suppress_errors=True)
-# subject.Weaning.populate(suppress_errors=True)
-subject.Death.populate(subj_res, suppress_errors=True)
-# subject.GenotypeTest.populate(suppress_errors=True)
-# subject.Zygosity.populate(suppress_errors=True)
+subject.Subject.populate(subj_res, **kargs)
+# subject.BreedingPair.populate(**kargs)
+# subject.Litter.populate(**kargs)
+# subject.LitterSubject.populate(**kargs)
+subject.SubjectProject.populate(subj_res, **kargs)
+subject.SubjectUser.populate(subj_res, **kargs)
+subject.SubjectLab.populate(subj_res, **kargs)
+# subject.Caging.populate(**kargs)
+# subject.UserHistory.populate(**kargs)
+# subject.Weaning.populate(**kargs)
+subject.Death.populate(subj_res, **kargs)
+# subject.GenotypeTest.populate(**kargs)
+# subject.Zygosity.populate(**kargs)
 
 # action tables
-action.ProcedureType.populate(suppress_errors=True)
+print('-------- Populating action shadow tables -----------')
+action.ProcedureType.populate(**kargs)
 action.Weighing.populate(
     utils.get_uuids('actions.weighing', 'weigh_uuid', subject_uuids),
-    suppress_errors=True)
-action.WaterType.populate(suppress_errors=True)
+    **kargs)
+action.WaterType.populate(**kargs)
+print('-------- Populating water administration entries -----------')
 action.WaterAdministration.populate(
     utils.get_uuids('actions.wateradministration',
                     'wateradmin_uuid', subject_uuids),
-    suppress_errors=True)
+    **kargs)
+print('-------- Populating water restriction entries -----------')
 action.WaterRestriction.populate(
     utils.get_uuids('actions.waterrestriction',
                     'restriction_uuid', subject_uuids),
-    suppress_errors=True)
+    **kargs)
 action.Surgery.populate(
     utils.get_uuids('actions.surgery', 'surgery_uuid', subject_uuids),
-    suppress_errors=True)
+    **kargs)
 
 
 # acquisition tables
+print('-------- Populating session entries -----------')
 acquisition.Session.populate(
     utils.get_uuids('actions.session', 'session_uuid', subject_uuids),
-    suppress_errors=True)
+    **kargs)
 
 # data tables
-data.DataFormat.populate(suppress_errors=True)
-data.DataRepositoryType.populate(suppress_errors=True)
-data.DataRepository.populate(suppress_errors=True)
-data.DataSetType.populate(suppress_errors=True)
+print('-------- Populating data shadow tables -----------')
+data.DataFormat.populate(**kargs)
+data.DataRepositoryType.populate(**kargs)
+data.DataRepository.populate(**kargs)
+data.DataSetType.populate(**kargs)
+print('-------- Populating dataset entries -----------')
 data.DataSet.populate(
     utils.get_uuids('data.dataset', 'dataset_uuid', subject_uuids),
-    suppress_errors=True)
+    **kargs)
+print('-------- Populating file record entries ----------')
 data.FileRecord.populate(
     utils.get_uuids('data.filerecord', 'record_uuid', subject_uuids),
-    suppress_errors=True)
+    **kargs)
