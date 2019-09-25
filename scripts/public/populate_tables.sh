4048bb9b9689ed59db8915660bd1a2ef0f27bfa9
shenshan <shenshanpku@gmail.com>
2019-09-25 15:03:27 -0700

    populate the tables other than filerecord.

---
+++ scripts/public/populate_tables.sh
@@ -0,0 +1,8 @@
+python ingest_alyx_shadow.py
+python ingest_alyx_shadow_membership.py
+python ../ingest_alyx_real.py
+python ../ingest_behavior.py
+python ../analyses_behavior.py
+python ../compute_latest_date.py
+python ../plotting_behavior.py
+python ../insert_update_subject_last_date.py
