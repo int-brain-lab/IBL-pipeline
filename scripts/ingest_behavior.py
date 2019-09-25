842d250f5aae706b2129856779d0e253e19d358e
shenshan <shenshanpku@gmail.com>
2019-09-25 15:57:12 -0700

    Add indicators for populate

--- scripts/ingest_behavior.py
+++ scripts/ingest_behavior.py
@@ -8,9 +8,15 @@ kargs = dict(
     suppress_errors=True, display_progress=True
 )
 
+print('-------- Populating CompleteWheelMoveSession -------')
 behavior.CompleteWheelMoveSession.populate(**kargs)
+print('-------------- Populating WheelMoveSet -------------')
 behavior.WheelMoveSet.populate(**kargs)
+print('---------- Populating CompleteTrialSession ---------')
 behavior.CompleteTrialSession.populate(**kargs)
+print('---------------- Populating TrialSet ---------------')
 behavior.TrialSet.populate(**kargs)
+print('------------ Populating AmbientSensorData ----------')
 behavior.AmbientSensorData.populate(**kargs)
+print('---------------- Populating Settings ---------------')
 behavior.Settings.populate(**kargs)
