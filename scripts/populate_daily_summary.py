268d54e63822d7b2b0cc207faf35ff9dd6ab8b5b
shenshan <shenshanpku@gmail.com>
2019-09-29 15:40:20 -0700

    Remove daily summary temporally for updates/

--- scripts/populate_daily_summary.py
+++ scripts/populate_daily_summary.py
@@ -1,8 +1,14 @@
 
 from ibl_pipeline.plotting import behavior
+from ibl_pipeline import reference
+
+dj.config['safemode'] = False
 
 kargs = dict(
     suppress_errors=True, display_progress=True
 )
 print('------ Populating plotting.DailyLabSummary ------')
+(behavior.DailyLabSummary & reference.Lab.aggr(
+    behavior.DailyLabSummary,
+    last_session_time='max(last_session_time)')).delete()
 behavior.DailyLabSummary.populate(**kargs)
