17b8921fb2d7ef7bcc9cf9da010812a761e55efd
shenshan <shenshanpku@gmail.com>
2019-10-02 14:22:42 -0700

    Compute reaction time for datasets with partial stim on time

--- ibl_pipeline/plotting/behavior.py
+++ ibl_pipeline/plotting/behavior.py
@@ -62,11 +62,11 @@ class SessionReactionTimeTrialNumber(dj.Computed):
 
     key_source = behavior_ingest.TrialSet & \
         (behavior_ingest.CompleteTrialSession &
-            'stim_on_times_status="Complete"')
+            'stim_on_times_status in ("Complete", "Partial")')
 
     def make(self, key):
         # get all trial of the session
-        trials = behavior_ingest.TrialSet.Trial & key
+        trials = behavior_ingest.TrialSet.Trial & key & 'trial_stim_on_time is not NULL'
         fig = putils.create_rt_trialnum_plot(trials)
         key['plotting_data'] = fig.to_plotly_json()
         self.insert1(key)
