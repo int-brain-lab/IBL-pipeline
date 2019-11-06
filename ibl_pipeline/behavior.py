ae6516f12c1777c24d0df5ec7207f81c410331f2
shenshan <shenshanpku@gmail.com>
2019-11-05 18:37:36 -0800

    SessionDelay

--- ibl_pipeline/behavior.py
+++ ibl_pipeline/behavior.py
@@ -712,9 +712,10 @@ class SessionDelay(dj.Imported):
         first_trial_duration = trial_end - trial_start
         elapsed_time = data[0][0]['elapsed_time'].split(':')
 
-        key['session_delay_in_secs'] = float(elapsed_time[1])*60 + float(elapsed_time[2]) - first_trial_duration
-        key['session_delay_in_mins'] = key['session_delay_in_secs']/60
-        self.insert1(key)
+        if elapsed_time:
+            key['session_delay_in_secs'] = float(elapsed_time[1])*60 + float(elapsed_time[2]) - first_trial_duration
+            key['session_delay_in_mins'] = key['session_delay_in_secs']/60
+            self.insert1(key)
 
 
 @schema
