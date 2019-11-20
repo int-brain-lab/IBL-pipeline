7fedff2d8f3cd143a604395619033d938caa591e
shenshan <shenshanpku@gmail.com>
2019-11-20 14:50:57 -0800

    plotting updates for trials with go cue trigger time

--- ibl_pipeline/plotting/behavior.py
+++ ibl_pipeline/plotting/behavior.py
@@ -123,7 +123,8 @@ class DateReactionTimeTrialNumber(dj.Computed):
     def make(self, key):
         trial_sets = (behavior_ingest.TrialSet &
                       (behavior_ingest.CompleteTrialSession &
-                       'stim_on_times_status in ("Complete", "Partial")')).proj(
+                       'stim_on_times_status in ("Complete", "Partial") or \
+                        go_trigger_time_status in ("Complete", "Partial")')).proj(
                 session_date='DATE(session_start_time)')
         trials = behavior_ingest.TrialSet.Trial & \
             (behavior_ingest.TrialSet * trial_sets & key)
