ee47f1b6f30089d5e6ce4a242b21024f01971174
shenshan <shenshanpku@gmail.com>
2019-11-20 13:53:03 -0800

    Test plotting for SessionReactionTime

--- scripts/tests/test_reaction_time_correction.py
+++ scripts/tests/test_reaction_time_correction.py
@@ -7,6 +7,7 @@ Shan Shen, Dec, 2019
 
 from ibl_pipeline import subject, acquisition, behavior
 from ibl_pipeline.analyses import behavior as behavior_analyses
+from ibl_pipeline.plotting import behavior as behavior_plotting
 
 subj = subject.Subject & 'subject_nickname="SWC_018"'
 
@@ -15,4 +16,8 @@ behavior_analyses.ReactionTime.populate(subj, display_progress=True)
 
 print('Testing ReactionTimeContrastBlock...')
 behavior_analyses.ReactionTimeContrastBlock.populate(
+    subj, display_progress=True)
+print('Testing plotting functions for \
+    SessionReactionTimeContrast and SessionReactionTimeTrialNumber')
+behavior_plotting.SessionReactionTimeContrast.populate(
     subj, display_progress=True)
