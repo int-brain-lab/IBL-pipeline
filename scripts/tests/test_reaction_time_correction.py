99e0efe7fc0d9e7a11aa1017649638bd9dd3dd51
shenshan <shenshanpku@gmail.com>
2019-11-20 12:02:28 -0800

    Test codes.

---
+++ scripts/tests/test_reaction_time_correction.py
@@ -0,0 +1,14 @@
+'''
+This script tests the modification on using go_cue_trigger_time
+for RT computation with stim_on_time is not available
+
+Shan Shen, Dec, 2019
+'''
+
+from ibl_pipeline import subject, acquisition, behavior
+from ibl_pipeline.analyses import behavior as behavior_analyses
+
+subj = subject.Subject & 'subject_nickname="SWC_018"'
+
+print('Testing ReactionTime...')
+behavior_analyses.ReactionTime.populate(subj, display_progress=True)
