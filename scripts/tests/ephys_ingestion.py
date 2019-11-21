5e01812247d5174aa4bbd462ddefe21a93ffab4e
shenshan <shenshanpku@gmail.com>
2019-11-20 18:09:49 -0800

    Ephys pipeline modification, test code for ephys ingestion.

--- scripts/tests/ephys_ingestion.py
+++ scripts/tests/ephys_ingestion.py
@@ -1,3 +1,10 @@
 '''
 This script tests the ingestion of ephys pipeline.
+
+Shan Shen, 2019-11-20
 '''
+
+from ibl_pipeline import subject, acquisition, behavior, ephys
+
+print('Testing ingestion of ProbeInsertion...')
+ephys.ProbeInsertion.populate(display_progress=True)
