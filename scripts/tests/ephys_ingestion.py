dfc9cf02171a92884d45bd61a18cbf28015477a3
shenshan <shenshanpku@gmail.com>
2019-11-23 16:53:16 -0800

    Test plotting ephys

--- scripts/tests/ephys_ingestion.py
+++ scripts/tests/ephys_ingestion.py
@@ -6,12 +6,22 @@ Shan Shen, 2019-11-20
 
 from ibl_pipeline import subject, acquisition, behavior, ephys
 
-print('Testing ingestion of ProbeInsertion...')
-ephys.ProbeInsertion.populate(display_progress=True)
+from ibl_pipeline.plotting import ephys as ephys_plotting
 
-print('Testing ingestion of ChannelGroup...')
-ephys.ChannelGroup.populate(display_progress=True, suppress_errors=True)
+# print('Testing ingestion of ProbeInsertion...')
+# ephys.ProbeInsertion.populate(display_progress=True)
+
+# print('Testing ingestion of ChannelGroup...')
+# ephys.ChannelGroup.populate(display_progress=True, suppress_errors=True)
 
 
-print('Testing ingestion of Cluster...')
-ephys.Cluster.populate(display_progress=True, suppress_errors=True)
+# print('Testing ingestion of Cluster...')
+# ephys.Cluster.populate(display_progress=True, suppress_errors=True)
+
+print('Testing ingestion of plotting raster...')
+ephys_plotting.RasterLinkOnly.populate(
+    display_progress=True, suppress_errors=True)
+
+print('Testing ingestion of plotting psth')
+ephys_plotting.PsthData.populate(
+    display_progress=True, suppress_errors=True)
