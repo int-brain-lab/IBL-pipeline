fe84546319eedd23629606411486635deaad318b
shenshan <shenshanpku@gmail.com>
2020-01-14 20:51:40 -0800

    Load public subjects

---
+++ scripts/public/delete_non_sharing_entries.py
@@ -0,0 +1,12 @@
+'''
+This script deletes the entries that are not shared in the public release of Jan 2020
+'''
+
+from ibl_pipeline import reference, subject, action, acquisition, data
+
+
+# delete all subjects that are not in the list
+(subject.Subject - public.PublicSubjectUuid).delete()
+
+# delete all sessions that are not in the list
+(acquisition.Session & 'session_start_time>"2019-12-01"').delete()
