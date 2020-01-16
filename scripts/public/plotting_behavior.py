89e7074dd699b451939b81633dcc21a3f0045fbd
shenshan <shenshanpku@gmail.com>
2020-01-15 22:15:52 -0800

    Populate plotting tables.

---
+++ scripts/public/plotting_behavior.py
@@ -0,0 +1,32 @@
+'''
+This script populate all behavioral plotting table for the website
+'''
+
+import datajoint as dj
+from ibl_pipeline.plotting import behavior
+from ibl_pipeline import subject
+
+dj.config['safemode'] = False
+
+kargs = dict(
+    suppress_errors=True, display_progress=True, reserve_jobs=True
+)
+
+print('------------ Populating plotting.SessionPsychCurve -----------')
+behavior.SessionPsychCurve.populate(**kargs)
+print('------ Populating plotting.SessionReactionTimeContrast -------')
+behavior.SessionReactionTimeContrast.populate(**kargs)
+print('---- Populating plotting.SessionReactionTimeTrialNumber ------')
+behavior.SessionReactionTimeTrialNumber.populate(**kargs)
+print('--------------- Populating plotting.DatePsychCurve -----------')
+behavior.DatePsychCurve.populate(**kargs)
+print('-------- Populating plotting.DateReactionTimeContrast --------')
+behavior.DateReactionTimeContrast.populate(**kargs)
+print('--------------- Populating plotting.WaterTypeColor -----------')
+behavior.WaterTypeColor.populate(**kargs)
+print('------------ Populating plotting.CumulativeSummary -----------')
+latest = subject.Subject.aggr(
+        behavior.LatestDate,
+        checking_ts='MAX(checking_ts)') * behavior.LatestDate
+(behavior.CumulativeSummary & latest).delete()
+behavior.CumulativeSummary.populate(**kargs)
