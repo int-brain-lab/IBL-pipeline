3e5869553014f9788e5c698dbd47df35b60f2f8b
shenshan <shenshanpku@gmail.com>
2020-02-09 15:17:13 -0800

    Update old ephys sessions one by one

---
+++ scripts/updates/delete_and_repopulate_old_ephys_sessions.py
@@ -0,0 +1,55 @@
+'''
+This script delete the rasters and psth plots from the old ephys sessions, and repopulate,
+from most recent to oldest
+'''
+
+
+import datajoint as dj
+from ibl_pipeline import ephys
+from ibl_pipeline.plotting import ephys as ephys_plotting
+from tqdm import tqdm
+from uuid import UUID
+import datetime
+
+
+dj.config['safemode'] = False
+
+# keys = (ephys.CompleteClusterSession &
+#         (ephys_plotting.RasterLinkS3 & 'session_start_time < "2020-02-04"')).fetch(
+#             'KEY', order_by='session_start_time desc')
+
+keys = \
+    [{'subject_uuid': UUID('221b68e7-0014-46ae-b8af-308665d8b478'),
+     'session_start_time': datetime.datetime(2020, 2, 3, 13, 49, 31)},
+     {'subject_uuid': UUID('d528fe0e-ac52-4fdc-bfb9-c545e44ded66'),
+     'session_start_time': datetime.datetime(2020, 2, 3, 12, 31, 9)}]
+
+for key in tqdm(keys):
+    print(key)
+    # delete tables
+    print('deleting entries from TrialSpikes cluster by cluster...')
+    # delete from TrialSpikes cluster by cluster
+    clusters = (ephys.Cluster & key).fetch('KEY')
+
+    for cluster in tqdm(clusters):
+        (ephys.TrialSpikes & cluster).delete()
+
+    print('repopulating TrialSpikes...')
+    ephys.TrialSpikes.populate(
+        key, display_progress=True,
+        suppress_errors=True)
+
+    print('deleting entries from Raster...')
+    (ephys_plotting.RasterLinkS3 & key).delete()
+
+    print('repopulating Raster...')
+    ephys_plotting.RasterLinkS3.populate(
+        key, display_progress=True,
+        suppress_errors=True)
+
+    print('deleting entries from Psth...')
+    (ephys_plotting.PsthDataVarchar & key).delete()
+
+    ephys_plotting.PsthDataVarchar.populate(
+        key, display_progress=True,
+        suppress_errors=True)
