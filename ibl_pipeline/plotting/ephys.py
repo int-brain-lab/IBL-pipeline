f5c6b79482eb2f97b3f7da6d72d0689b38ab0118
shenshan <shenshanpku@gmail.com>
2020-02-12 17:36:26 -0800

    Change the Valid Align sort

--- ibl_pipeline/plotting/ephys.py
+++ ibl_pipeline/plotting/ephys.py
@@ -20,7 +20,9 @@ class Sorting(dj.Lookup):
     contents = zip(['trial_id',
                     'response - stim on',
                     'feedback - stim on',
-                    'feedback - response'])
+                    'feedback - response',
+                    'contrast',
+                    'feedback type'])
 
 
 @schema
@@ -31,14 +33,10 @@ class ValidAlignSort(dj.Lookup):
     """
     contents = [
         ['stim on', 'trial_id'],
-        ['stim on', 'response - stim on'],
+        ['stim on', 'contrast'],
         ['stim on', 'feedback - stim on'],
-        ['response', 'trial_id'],
-        ['response', 'response - stim on'],
-        ['response', 'feedback - response'],
         ['feedback', 'trial_id'],
-        ['feedback', 'feedback - stim on'],
-        ['feedback', 'feedback - response']
+        ['feedback', 'feedback type']
     ]
 
 
@@ -52,110 +50,6 @@ class TrialCondition(dj.Lookup):
                     'correct trials',
                     'left trials',
                     'right trials'])
-
-
-@schema
-class Raster(dj.Computed):
-    definition = """
-    -> ephys.Cluster
-    -> ValidAlignSort
-    ---
-    plotting_data:      blob@plotting
-    """
-    key_source = ephys.Cluster * ValidAlignSort & behavior.TrialSet & ephys.TrialSpikes
-
-    def make(self, key):
-        cluster = ephys.Cluster & key
-        trials = \
-            (behavior.TrialSet.Trial * ephys.TrialSpikes & cluster).proj(
-                'trial_start_time', 'trial_stim_on_time',
-                'trial_response_time',
-                'trial_feedback_time',
-                'trial_response_choice',
-                'trial_spike_times',
-                trial_duration='trial_end_time-trial_start_time',
-                trial_signed_contrast="""trial_stim_contrast_right -
-                                         trial_stim_contrast_left"""
-            ) & 'trial_duration < 5' & 'trial_response_choice!="No Go"'
-
-        if not len(trials):
-            return
-        align_event = (ephys.Event & key).fetch1('event')
-        sorting_var = (Sorting & key).fetch1('sort_by')
-        x_lim = [-1, 1]
-        encoded_string, y_lim, label = putils.create_raster_plot_combined(
-            trials, align_event, sorting_var)
-
-        axis = go.Scatter(
-            x=x_lim,
-            y=y_lim,
-            mode='markers',
-            marker=dict(opacity=0),
-            showlegend=False
-        )
-
-        legend_left = putils.get_legend('left', 'spike')
-        legend_right = putils.get_legend('right', 'spike')
-        legend_incorrect = putils.get_legend('incorrect', 'spike')
-
-        if sorting_var != 'trial_id':
-            legend_mark_left = putils.get_legend('left', label)
-            legend_mark_right = putils.get_legend('right', label)
-            legend_mark_incorrect = putils.get_legend('incorrect', label)
-
-        layout = go.Layout(
-            images=[dict(
-                source='data:image/png;base64, ' + encoded_string.decode(),
-                sizex=x_lim[1] - x_lim[0],
-                sizey=y_lim[1] - y_lim[0],
-                x=x_lim[0],
-                y=y_lim[1],
-                xref='x',
-                yref='y',
-                sizing='stretch',
-                layer='below'
-                )],
-            width=580,
-            height=370,
-            margin=go.layout.Margin(
-                l=50,
-                r=30,
-                b=40,
-                t=80,
-                pad=0
-            ),
-            title=dict(
-                text='Raster, aligned to {}'.format(align_event),
-                x=0.21,
-                y=0.87
-            ),
-            xaxis=dict(
-                title='Time (sec)',
-                range=x_lim,
-                showgrid=False
-            ),
-            yaxis=dict(
-                title='Trial idx',
-                range=y_lim,
-                showgrid=False
-            ),
-        #     template=dict(
-        #         layout=dict(
-        #             plot_bgcolor="#fff"
-        #         )
-        #     )
-        )
-
-        if sorting_var != 'trial_id':
-            data = [axis, legend_left, legend_right, legend_incorrect,
-                    legend_mark_left, legend_mark_right, legend_mark_incorrect]
-        else:
-            data = [axis, legend_left, legend_right, legend_incorrect]
-
-        fig = go.Figure(data=data, layout=layout)
-        key['plotting_data'] = fig.to_plotly_json()
-
-        self.insert1(key)
 
 
 @schema
@@ -234,6 +128,20 @@ class RasterLayoutTemplate(dj.Lookup):
         template_1,
         template_2
     ]
+
+
+@schema
+class Raster(dj.Computed):
+    definition = """
+    -> ephys.Cluster
+    -> ValidAlignSort
+    ---
+    plotting_data:      blob@plotting
+    """
+    key_source = ephys.Cluster * ValidAlignSort & behavior.TrialSet & ephys.TrialSpikes
+
+    def make(self, key):
+        pass
 
 
 @schema
