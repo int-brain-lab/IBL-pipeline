87bd81f1ec643304be8601aae240fc8253698a42
shenshan <shenshanpku@gmail.com>
2020-02-21 17:50:10 -0800

    Add timers to ingest script

--- scripts/ingest_all.sh
+++ scripts/ingest_all.sh
@@ -1,9 +1,18 @@
 python delete_shadow_tables_for_updates.py
+date
 python ingest_alyx_raw.py "$@"
+date
 python ingest_alyx_shadow.py
+date
 python ingest_alyx_shadow_membership.py
+date
 python delete_real_tables_for_updates.py
+date
 python ingest_alyx_real.py
+date
 python ingest_behavior.py
+date
 python analyses_behavior.py
+date
 python plotting_behavior.py
+date
