b12f26671611c6a9d713b50c618b3df08d68fe7a
shenshan <shenshanpku@gmail.com>
2020-03-10 19:23:40 -0700

    Filter out long json fields to ingest

--- scripts/ingest_alyx_raw.py
+++ scripts/ingest_alyx_raw.py
@@ -52,7 +52,7 @@ for key in keys:
         key_field = dict(key_field, fname=field_name)
 
         if field_name == 'json' and field_value is not None:
-            if len(field_value) > 4000:
+            if len(field_value) > 1000:
                 continue
             else:
                 key_field['value_idx'] = 0
