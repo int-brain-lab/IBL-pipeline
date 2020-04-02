c82905c191f67d7df128ca2b6163b85b9eee515a
shenshan <shenshanpku@gmail.com>
2020-04-01 21:32:13 -0700

    Add housing tables

--- ibl_pipeline/ingest/subject.py
+++ ibl_pipeline/ingest/subject.py
@@ -729,8 +729,8 @@ class Housing(dj.Computed):
             key_housing['cage_clean_frequency'] = frequency
 
         light_cycle = grf(key, 'light_cycle')
-        if light_cycle and light_cycle != 'None':
-            key_housing['light_cycle'] = frequency
+        if light_cycle != 'None':
+            key_housing['light_cycle'] = light_cycle
 
         description = grf(key, 'description')
         if description != 'None':
