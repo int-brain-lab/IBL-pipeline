69f8edb298faa53bc0e9087c9d27f356af31465c
shenshan <shenshanpku@gmail.com>
2020-04-28 22:07:35 -0700

    Update null trial fields

--- scripts/updates/update_trial_fields.py
+++ scripts/updates/update_trial_fields.py
@@ -32,10 +32,16 @@ def update_field(key, eID, trials, alf, djattr, dtype, status,
 
 # fetch keys with reward_volume but null in table TrialSet.Trial
 
-keys = (behavior.TrialSet &
-        (behavior.TrialSet.Trial & 'trial_reward_volume is null') &
-        (behavior.CompleteTrialSession &
-         'reward_volume_status="Complete"')).fetch('KEY')
+if len(sys.argvf) < 2:
+    keys = (behavior.TrialSet &
+            (behavior.TrialSet.Trial & 'trial_reward_volume is null') &
+            (behavior.CompleteTrialSession &
+            'reward_volume_status="Complete"')).fetch('KEY')
+else:
+    keys = (behavior.TrialSet & sys.argv[1] &
+            (behavior.TrialSet.Trial & 'trial_reward_volume is null') &
+            (behavior.CompleteTrialSession &
+            'reward_volume_status="Complete"')).fetch('KEY')
 
 fields = [
     {'alf': 'trials.repNum',             'djattr': 'trial_rep_num',             'dtype': int,   'status': 'rep_num_status'},
