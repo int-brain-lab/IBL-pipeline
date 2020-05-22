2d160104dfc4433885241424aa9fc28d5aee789d
shenshan <shenshanpku@gmail.com>
2020-05-22 15:30:16 -0700

    Add movement alignment to depth peth

--- ibl_pipeline/analyses/ephys.py
+++ ibl_pipeline/analyses/ephys.py
@@ -100,7 +100,7 @@ class DepthPeth(dj.Computed):
             key.update(depth_peth=np.vstack(peth_list),
                        depth_baseline=np.array(baseline_list),
                        time_bin_centers=peths.tscale)
-            self.insert1(key)
+            self.insert1(key, skip_duplicates=True)
 
 
 @schema
