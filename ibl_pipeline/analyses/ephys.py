560124d603abfbf0695c85bb04702bab94bb9b30
shenshan <shenshanpku@gmail.com>
2020-05-22 15:26:03 -0700

    Add movement alignment to depth peth

--- ibl_pipeline/analyses/ephys.py
+++ ibl_pipeline/analyses/ephys.py
@@ -8,6 +8,8 @@ import numpy as np
 schema = dj.schema(dj.config.get('database.prefix', '') +
                    'ibl_analyses_ephys')
 
+wheel = dj.create_virtual_module('wheel', 'group_shared_wheel')
+
 
 @schema
 class TrialType(dj.Lookup):
@@ -36,7 +38,7 @@ class DepthPeth(dj.Computed):
     """
     key_source = ephys.ProbeInsertion * ephys.Event * \
         (TrialType & 'trial_type="Correct All"') & ephys.DefaultCluster & \
-        behavior.TrialSet & 'event in ("stim on", "feedback", "movement")'
+        behavior.TrialSet & wheel.MovementTimes & 'event in ("stim on", "feedback", "movement")'
 
     def make(self, key):
 
@@ -51,7 +53,7 @@ class DepthPeth(dj.Computed):
              for (cluster_id, cluster_spk_depths) in zip(clusters_ids,
                                                          clusters_spk_depths)])
 
-        trials = (behavior.TrialSet.Trial & key &
+        trials = (behavior.TrialSet.Trial * wheel.MovementTimes & key &
                   'trial_feedback_type=1').fetch()
 
         bin_size_depth = 80
@@ -64,45 +66,41 @@ class DepthPeth(dj.Computed):
         key.update(trial_type='Correct All',
                    depth_bin_centers=(edges[:-1] + edges[1:])/2)
 
-        events = ['stim on', 'feedback']
+        if event == 'feedback':
+            event_times = trials['trial_feedback_time']
+        elif event == 'stim on':
+            event_times = trials['trial_stim_on_time']
+        elif event == 'movement':
+            event_times = trials['movement_onset']
 
-        for event in events:
-            if event == 'feedback':
-                event_times = trials['trial_feedback_time']
-            elif event == 'stim on':
-                event_times = trials['trial_stim_on_time']
+        peth_list = []
+        baseline_list = []
 
-            key.update(event=event)
+        for i in tqdm(np.arange(len(bin_edges)) + 1):
+            f = spk_bin_ids == i
+            spikes_ibin = spikes_times[f]
+            spike_clusters = spikes_clusters[f]
+            cluster_ids = np.unique(spike_clusters)
 
-            peth_list = []
-            baseline_list = []
+            peths, binned_spikes = bb.singlecell.calculate_peths(
+                spikes_ibin, spike_clusters, cluster_ids,
+                event_times, pre_time=0.3, post_time=1)
+            if len(peths.means):
+                time = peths.tscale
+                peth = np.sum(peths.means, axis=0)
+                baseline = peth[np.logical_and(time > -0.3, time < 0)]
+                mean_bsl = np.mean(baseline)
 
-            for i in tqdm(np.arange(len(bin_edges)) + 1):
-                f = spk_bin_ids == i
-                spikes_ibin = spikes_times[f]
-                spike_clusters = spikes_clusters[f]
-                cluster_ids = np.unique(spike_clusters)
+                peth_list.append(peth)
+                baseline_list.append(mean_bsl)
+            else:
+                peth_list.append(np.zeros_like(peths.tscale))
+                baseline_list.append(0)
 
-                peths, binned_spikes = bb.singlecell.calculate_peths(
-                    spikes_ibin, spike_clusters, cluster_ids,
-                    event_times, pre_time=0.3, post_time=1)
-                if len(peths.means):
-                    time = peths.tscale
-                    peth = np.sum(peths.means, axis=0)
-                    baseline = peth[np.logical_and(time > -0.3, time < 0)]
-                    mean_bsl = np.mean(baseline)
-
-                    peth_list.append(peth)
-                    baseline_list.append(mean_bsl)
-                else:
-                    peth_list.append(np.zeros_like(peths.tscale))
-                    baseline_list.append(0)
-
-            key.update(event=event,
-                       depth_peth=np.vstack(peth_list),
+            key.update(depth_peth=np.vstack(peth_list),
                        depth_baseline=np.array(baseline_list),
                        time_bin_centers=peths.tscale)
-            self.insert1(key, skip_duplicates=True)
+            self.insert1(key)
 
 
 @schema
