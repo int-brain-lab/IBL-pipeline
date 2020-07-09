17b7acf8e953a549ee30300f0d34bd48163e419e
shenshan <shenshanpku@gmail.com>
2020-07-09 16:13:14 -0700

    add utility dependent tables

---
+++ ibl_pipeline/utils/dependent_tables.py
@@ -0,0 +1,36 @@
+import datajoint as dj
+import ibl_pipeline
+import inspect
+import re
+
+
+class Graph():
+
+    def __init__(self, table):
+
+        self.table = table
+        self.graph = table.connection.dependencies
+        self.graph.load()
+        self.descendants = self.graph.descendants(table.full_table_name)
+
+    @property
+    def table_list(self):
+
+        context = inspect.currentframe().f_back.f_locals
+        table_list = []
+        for t_db in self.descendants:
+            try:
+                int(t_db)
+                continue
+            except Exception as e:
+                pass
+            t = dj.table.lookup_class_name(t_db, context)
+
+            if not t:
+                schema_name = re.match('`(.*)`\.', t_db).group(1)
+                vmod = dj.create_virtual_module(schema_name, schema_name)
+                context[vmod.__name__] = vmod
+                t = dj.table.lookup_class_name(
+                    t_db, context)
+
+            table_list.append(t)
