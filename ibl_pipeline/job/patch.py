1e67a603fd58c11b067b80b34efc9aea7123f6bd
shenshan <shenshanpku@gmail.com>
2020-07-13 14:21:11 -0700

    Patch job management pipelien

--- ibl_pipeline/job/patch.py
+++ ibl_pipeline/job/patch.py
@@ -6,6 +6,8 @@ from ibl_pipeline.plotting import behavior as behavior_plotting
 from ibl_pipeline.plotting import ephys as ephys_plotting
 from ibl_pipeline.group_shared import wheel
 from ibl_pipeline.utils.dependent_tables import Graph
+import ibl_pipeline
+import re
 
 
 schema = dj.schema(dj.config.get('database.prefix', '') + 'ibl_patch')
@@ -48,6 +50,7 @@ TABLES = [
 @schema
 class Session(dj.Manual):
     definition = """
+    job_date            : date
     subject_uuid        : uuid
     session_start_time  : datetime
     ---
@@ -65,6 +68,134 @@ class Table(dj.Lookup):
     table_order     : smallint      # order to repopulate, the bigger, the earlier to delete and later to repopulate
     table_label     : enum('virtual', 'auto', 'manual')  # virtual for virtual module, auto for computed or imported table, manual for manual table
     """
+
+
+table_kwargs = dict(order_by='table_order desc', as_dict=True)
+
+TABLES_PACKAGE = (Table & 'table_label!="virtual"').fetch(**table_kwargs)
+TABLES_VIRTUAL = (Table & 'table_lable="virtual"').fetch(**table_kwargs)
+
+populate_kwargs = dict(
+    suppress_errors=True, display_progress=True,
+    return_exception_objects=True)
+
+
+@schema
+class Run(dj.Manual):
+    definition = """
+    # the table that drives deletion and repopulate
+    -> Session
+    """
+
+    def _delete_table(self, t, key, virtual=True):
+
+        if virtual:
+            Graph.get_virtual_module(t['full_table_name'])
+
+        table_class = eval(t['table_class'])
+        key_table = dict(**key, full_table_name=t['full_table_name'])
+
+        if len(table_class & key):
+            original = True
+        else:
+            original = False
+
+        RunStatus.TableStatus.insert1(
+            dict(**key_table, original=original))
+
+        print('Deleting table {} ...'.format(t['full_table_name']))
+        (table_class & key).delete_quick()
+        dj.Table._update(
+            RunStatus.TableStatus & key_table,
+            'status', 'Deleted')
+        dj.Table._update(
+            RunStatus.TableStatus & key_table,
+            'delete_time', datetime.datetime.now())
+
+    def make(self, key):
+
+        # start this job
+        if not len(RunStatus & key):
+            RunStatus.insert1(
+                **key,
+                run_start_time=datetime.datetime.now())
+        else:
+            dj.Table._update(
+                RunStatus & key,
+                'run_start_time', datetime.datetime.now())
+
+        # delete tables
+        for t in TABLES_VIRTUAL:
+            self._delete_table(t, key)
+
+        for t in TABLES_PACKAGE:
+            self._delete_table(t, key, virtual=False)
+
+        # repopulate tables
+        for t in TABLES_PACKAGE[::-1]:
+            if t['table_label'] == 'auto':
+                table_class = eval(t['table_class'])
+                table_key = dict(**key, full_table_name=t['full_table_name'])
+                status = RunStatus.TableStatus & table_key
+                print('Repopulating {}'.format(t['table_class']))
+                dj.Table._update(status, 'status', 'Populating')
+                dj.Table._update(
+                    status, 'populate_start_time', datetime.datetime.now())
+
+                if not len((table_class.key_source - table_class.proj()) & key):
+                    dj.Table._update(status, 'status', 'Error')
+                    dj.Table._update(
+                        status, 'populate_end_time', datetime.datetime.now())
+                    dj.Table._update(
+                        status, 'error_message', 'No tuples to populate')
+                else:
+                    errors = table_class.populate(**populate_kwargs)
+                    dj.Table._update(
+                        status, 'populate_end_time', datetime.datetime.now())
+                    if len(errors):
+                        dj.Table._update(status, 'status', 'Error')
+                        dj.Table._update(
+                            status, 'error_message', str(errors))
+                    else:
+                        dj.Table._update(status, 'status', 'Success')
+                        dj.Table._update(
+                            status, 'error_message', '')
+
+        # end this job
+        dj.Table._update(
+            RunStatus & key,
+            'run_end_time', datetime.datetime.now())
+
+    def populate(self, *restrictions, display_progress=False):
+
+        self.key_source = (Session - self) & dj.AndList(restrictions)
+        keys = self.key_source.fetch('KEY')
+
+        for key in (tqdm(keys) if display_progress else keys):
+            self.make(key)
+
+
+@schema
+class RunStatus(dj.Manual):
+    definition = """
+    -> Session
+    ---
+    run_start_time          : datetime
+    run_end_time=null       : datetime
+    """
+
+    class TableStatus(dj.Part):
+        definition = """
+        -> master
+        -> Table
+        ---
+        original                : bool
+        status=null             : enum('Deleted', 'Repopulating', 'Success', 'Error')
+        delete_time=null        : datetime
+        populate_time=null : datetime
+        populate_done_time=null : datetime
+        error_message=''        : varchar(10000)
+        """
 
 
 if __name__ == '__main__':
