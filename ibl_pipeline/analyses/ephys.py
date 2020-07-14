4a5c5f0983aa3cad3d2af5ba5d61402d21e9b6b6
shenshan <shenshanpku@gmail.com>
2020-07-14 16:18:53 -0700

    Fix bug on depth peth

--- ibl_pipeline/analyses/ephys.py
+++ ibl_pipeline/analyses/ephys.py
@@ -97,10 +97,10 @@ class DepthPeth(dj.Computed):
                 peth_list.append(np.zeros_like(peths.tscale))
                 baseline_list.append(0)
 
-            key.update(depth_peth=np.vstack(peth_list),
-                       depth_baseline=np.array(baseline_list),
-                       time_bin_centers=peths.tscale)
-            self.insert1(key, skip_duplicates=True)
+        key.update(depth_peth=np.vstack(peth_list),
+                   depth_baseline=np.array(baseline_list),
+                   time_bin_centers=peths.tscale)
+        self.insert1(key, skip_duplicates=True)
 
 
 @schema
