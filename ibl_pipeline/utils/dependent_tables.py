95e0dd9dd2664f937a815c6578a22a6e65a7a16d
shenshan <shenshanpku@gmail.com>
2020-07-17 18:04:37 -0700

    Fix the context to search for dependent tables.

--- ibl_pipeline/utils/dependent_tables.py
+++ ibl_pipeline/utils/dependent_tables.py
@@ -1,5 +1,10 @@
 import datajoint as dj
-import ibl_pipeline
+from ibl_pipeline import acquisition, behavior, ephys, histology
+from ibl_pipeline.analyses import behavior as behavior_analyses
+from ibl_pipeline.analyses import ephys as ephys_analyses
+from ibl_pipeline.plotting import behavior as behavior_plotting
+from ibl_pipeline.plotting import ephys as ephys_plotting
+from ibl_pipeline.group_shared import wheel
 import inspect
 import re
 import pdb
@@ -22,10 +27,11 @@ class Graph():
         schema_name = re.match('`(.*)`\.', full_table_name).group(1)
         vmod = dj.create_virtual_module(schema_name, schema_name)
         context[vmod.__name__] = vmod
+        return vmod.__name__
 
     def get_table_list(self, virtual_only=False):
 
-        context = inspect.currentframe().f_back.f_globals
+        context = inspect.currentframe().f_globals
         table_list = []
         for t_db in self.descendants:
             try:
@@ -38,10 +44,11 @@ class Graph():
             if t:
                 table_list.append(dict(label='package', table=t))
             else:
-                Graph.get_virtual_module(t_db, context)
+                vmod_name = Graph.get_virtual_module(t_db, context)
                 t = dj.table.lookup_class_name(
                     t_db, context)
                 table_list.append(dict(label='virtual', table=t))
+                context.pop(vmod_name)
 
         if virtual_only:
             return [t['table'] for t in table_list if t['label'] == 'virtual']
