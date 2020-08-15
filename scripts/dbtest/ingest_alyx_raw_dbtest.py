e4d38c1d7199519fa92bcfd60d3109be67558765
Edgar Y. Walker <edgar.walker@gmail.com>
2020-08-15 00:19:05 +0000

    move sys.argv outside the function

--- scripts/dbtest/ingest_alyx_raw_dbtest.py
+++ scripts/dbtest/ingest_alyx_raw_dbtest.py
@@ -5,8 +5,8 @@ This script ingest alyxraw for test database, ignoring the data tables.
 from ibl_pipeline.ingest.ingest_alyx_raw import (get_alyx_entries,
                                                  insert_to_alyxraw)
 
-exclude_list = ['data.dataformat', 'data.datarepositorytype',
+exclude_list = {'data.dataformat', 'data.datarepositorytype',
                 'data.datasettype', 'data.dataset',
-                'data.filerecord']
+                'data.filerecord'}
 
 insert_to_alyxraw(get_alyx_entries(exclude=exclude_list))
