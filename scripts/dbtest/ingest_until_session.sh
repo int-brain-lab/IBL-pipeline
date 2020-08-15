a13f3337da7525f3bc4ea6a91b14f350931e3d8d
shenshan <shenshanpku@gmail.com>
2020-08-15 00:42:33 +0000

    Add echo messages.

--- scripts/dbtest/ingest_until_session.sh
+++ scripts/dbtest/ingest_until_session.sh
@@ -1,8 +1,13 @@
+date
+echo "Ingesting alyx raw..."
 python ingest_alyx_raw_dbtest.py
 date
+echo "Ingesting alyx shadow tables..."
 python ingest_alyx_shadow.py
 date
+echo "Ingesting alyx shadow membership tables..."
 python ingest_alyx_shadow_membership.py
 date
+echo "Copying shadow tables to real tables..."
 python ingest_alyx_real.py
 date
