55fbb6f1fd80ce20b4477545083bc1e6961bbdb3
shenshan <shenshanpku@gmail.com>
2020-09-16 19:38:49 +0000

    Add data module into the ibl_pipeline common

--- ibl_pipeline/common.py
+++ ibl_pipeline/common.py
@@ -1,4 +1,4 @@
-from ibl_pipeline import reference, subject, action, acquisition, behavior, ephys, histology
+from ibl_pipeline import reference, subject, action, acquisition, data, behavior, ephys, histology
 from ibl_pipeline.analyses import behavior as behavior_analyses
 from ibl_pipeline.analyses import ephys as ephys_analyses
 from ibl_pipeline.plotting import behavior as behavior_plotting
