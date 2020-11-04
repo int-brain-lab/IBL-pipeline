6f7d9f71bb8bb2a5bb3a51eec64e70be93fd4db3
shenshan <shenshanpku@gmail.com>
2020-11-04 19:48:37 +0000

    process histology

--- ibl_pipeline/process/process_histology.py
+++ ibl_pipeline/process/process_histology.py
@@ -1,9 +1,10 @@
-from ibl_pipeline.process import ingest_alyx_raw
+from ibl_pipeline.process import ingest_alyx_raw, ingest_real
 from ibl_pipeline.ingest.common import *
 from ibl_pipeline.ingest import populate_batch, InsertBuffer
 from ibl_pipeline.common import *
 from ibl_pipeline.process import update_utils
 from tqdm import tqdm
+import inspect
 
 
 ALYX_HISTOLOGY_MODELS = [
@@ -11,6 +12,7 @@ ALYX_HISTOLOGY_MODELS = [
     'subjects.project', 'subjects.species', 'subjects.strain', 'subjects.source',
     'subjects.allele', 'subjects.sequence', 'subjects.subject',
     'actions.proceduretype', 'actions.wateradministration', 'actions.session',
+    'experiments.probemodel',
     'experiments.probeinsertion', 'experiments.coordinatesystem',
     'experiments.trajectoryestimate', 'experiments.channel']
 
@@ -35,9 +37,22 @@ HISTOLOGY_SHADOW_TABLES = [
     action_ingest.ProcedureType,
     action_ingest.WaterAdministration,
     acquisition_ingest.Session,
+    ephys_ingest.ProbeModel,
     ephys_ingest.ProbeInsertion,
     histology_ingest.ProbeTrajectoryTemp,
     histology_ingest.ChannelBrainLocationTemp
+]
+
+HISTOLOGY_TABLES_FOR_DELETE = [
+    histology.ProbeBrainRegionTemp,
+    histology.ClusterBrainRegionTemp,
+    histology.ChannelBrainLocationTemp,
+    histology.ProbeTrajectoryTemp,
+]
+
+HISTOLOGY_TABLES_FOR_POPULATE = [
+    histology.ClusterBrainRegionTemp,
+    histology.ProbeBrainRegionTemp,
 ]
 
 
@@ -55,7 +70,7 @@ def process_alyxraw_histology(
     )
 
 
-def process_shadow_tables():
+def populate_shadow_tables():
 
     kwargs = dict(
         display_progress=True,
@@ -70,7 +85,7 @@ def process_shadow_tables():
             t.populate(**kwargs)
 
 
-def delete_histology_alyx_shadow():
+def delete_histology_alyx_shadow(verbose=False):
 
     CHANNEL_TABLES = [
         histology_ingest.ChannelBrainLocationTemp,
@@ -88,13 +103,13 @@ def delete_histology_alyx_shadow():
 
         for k in tqdm(keys, position=0):
             table.delete1(k)
-            if table.flush_delete(chunksz=1000, quick=True):
+            if table.flush_delete(chunksz=1000, quick=True) and verbose:
                 print(f'Deleted 1000 entries from {t.__name__}')
 
         table.flush_delete(quick=True)
 
     traj_keys = update_utils.get_deleted_keys('experiments.trajectoryestimate') + \
-                update_utils.get_updated_keys('experiments.trajectoryestimate')
+        update_utils.get_updated_keys('experiments.trajectoryestimate')
 
     TRAJ_TABLES = [
         histology_ingest.ProbeTrajectoryTemp,
@@ -109,20 +124,67 @@ def delete_histology_alyx_shadow():
         table = InsertBuffer(t)
         for k in tqdm(keys, position=0):
             table.delete1(k)
-            if table.flush_delete(chunksz=1000, quick=True):
+            if table.flush_delete(chunksz=1000, quick=True) and verbose:
                 print(f'Deleted 1000 entries from {t.__name__}')
         table.flush_delete(quick=True)
 
-# def process_real_tables():
 
-#     for shadow_table in HISTOLOGY_SHADOW_TABLES:
-#         real_table = getattr()
-#         print(f'Populating {t.__name__}')
-#         t.populate(**kwargs)
+def delete_histology_real():
+
+    traj_uuids = update_utils.get_deleted_keys('experiments.trajectoryestimate') + \
+        update_utils.get_updated_keys('experiments.trajectoryestimate')
+
+    traj_uuids_real = [
+        {'probe_trajectory_uuid': k['uuid']} for k in traj_uuids]
+
+    traj_keys = (histology.ProbeTrajectoryTemp & traj_uuids_real).fetch('KEY')
+
+    for t in HISTOLOGY_TABLES_FOR_DELETE:
+        print(f'Deleting from table {t.__name__}')
+        (t & traj_keys).delete_quick()
+
+
+def copy_to_real_tables():
+
+    for shadow_table in HISTOLOGY_SHADOW_TABLES:
+
+        mod = shadow_table.__module__
+        shadow_module = inspect.getmodule(shadow_table)
+        real_module = eval(mod.replace('ibl_pipeline.ingest.', ''))
+        table_name = shadow_table.__name__
+        print(f'Copying table {table_name}...')
+        ingest_real.copy_table(
+            real_module, shadow_module, table_name, allow_direct_insert=True)
+
+
+def populate_real_tables():
+
+    for t in HISTOLOGY_TABLES_FOR_POPULATE:
+        print(f'Populating {t.__name__}...')
+        t.populate(suppress_errors=True, display_progress=True)
+
+
+def main(fpath='/data/alyxfull.json'):
+
+    print('Insert to update alyxraw...')
+    update_utils.insert_to_update_alyxraw(
+        filename=fpath, delete_tables=True,
+        models=['experiments.trajectoryestimate', 'experiments.channel'])
+    print('Deleting from alyx and shadow...')
+    delete_histology_alyx_shadow()
+    print('Ingesting new alyxraw...')
+    process_alyxraw_histology(filename=fpath)
+    print('Populating new shadow...')
+    populate_shadow_tables()
+    print('Deleting real table entries...')
+    delete_histology_real()
+    print('Copying to real tables...')
+    copy_to_real_tables()
+    print('Populating real tables...')
+    populate_real_tables()
 
 
 if __name__ == '__main__':
 
-    process_alyxraw_histology(filename='/data/alyxfull_20201010_1200.json')
-    process_shadow_tables()
-    # delete_histology_alyx_shadow()
+    # fpath = '/data/alyxfull_20201016_1200.json'
+    main()
