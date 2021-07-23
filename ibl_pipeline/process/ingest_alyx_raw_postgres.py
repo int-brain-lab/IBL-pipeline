b321c4924f3dded78787f8e1d4e0b2e8b9305d8d
shenshan <shenshanpku@gmail.com>
2021-07-23 23:27:17 +0000

    move ingest to update alyxraw to ingest_alyx_raw_postgres

--- ibl_pipeline/process/ingest_alyx_raw_postgres.py
+++ ibl_pipeline/process/ingest_alyx_raw_postgres.py
@@ -1,10 +1,14 @@
 '''
 This script loads the data from alyx postgres database and insert the entries into the alyxraw table.
 '''
-import os, json, logging, math, datetime, re, django
+from ibl_pipeline.acquisition_internal import WaterAdministrationSession
+from ibl_pipeline.action_internal import WaterAdministration
+import os, json, logging, math, datetime, re, django, warnings
 from ibl_pipeline.ingest import alyxraw, QueryBuffer
 from tqdm import tqdm
 import numpy as np
+import datajoint as dj
+
 
 django.setup()
 
@@ -54,6 +58,10 @@ TABLES_OF_INTEREST = [
     actions.models.CullMethod,
     actions.models.CullReason,
     actions.models.Cull,
+    actions.models.Weighing,
+    actions.models.WaterType,
+    actions.models.WaterRestriction,
+    actions.models.WaterAdministration,
     actions.models.Session,
     # data.models
     data.models.DataFormat,
@@ -70,6 +78,18 @@ TABLES_OF_INTEREST = [
 ]
 
 
+def get_alyx_model_name(alyx_model):
+    """get alyx model name ("model" field in alyxraw.AlyxRaw) from an alyx model object
+
+    Args:
+        alyx_model (alyx model object): alyx model object, e.g. misc.models.Lab
+
+    Returns:
+        [str]: "model" field in alyxraw.AlyxRaw, e.g. misc.lab
+    """
+    # return alyx_model.__module__.split('.')[0] + '.' + alyx_model.__name__.lower()
+    return alyx_model._meta.db_table.replace('_', '.')
+
 def get_field_names(alyx_model):
     return [field.name for field in alyx_model._meta.fields]
 
@@ -83,12 +103,17 @@ def get_tables_with_auto_datetime(tables=None):
             if 'auto_datetime' in get_field_names(t)])
 
 
-def insert_alyx_entries_model(alyx_model, backtrack_days=None):
-    """Insert alyx entries for a particular alyx model
+def insert_alyx_entries_model(
+        alyx_model,
+        alyxraw_dj_module=alyxraw,
+        backtrack_days=None):
+    """Insert alyx entries into alyxraw tables for a particular alyx model
 
     Args:
         alyx_model (django.model object): alyx model
-        backtrack_days (int, optional): number of days the data are within to backtrack and ingest.
+        alyxraw_dj_module (datajoint module): datajoint module containing AlyxRaw tables, either alyxraw or alyxraw update
+        backtrack_days (int, optional): number of days the data are within to backtrack and ingest,
+            just applicable to tables with auto_datetime field
     """
     if backtrack_days:
         # only ingest the latest data
@@ -109,22 +134,31 @@ def insert_alyx_entries_model(alyx_model, backtrack_days=None):
         entries = alyx_model.objects.all()
 
     # ingest into main table
-    model_name = alyx_model._meta.db_table.replace('_', '.')
+    model_name = get_alyx_model_name(alyx_model)
     pk_list = entries.values_list('id', flat=True)
 
-    alyxraw.AlyxRaw.insert(
+    # This is not very slow, if too slow, use QueryBuffer instead
+    alyxraw_dj_module.AlyxRaw.insert(
         [dict(uuid=s, model=model_name) for s in pk_list],
         skip_duplicates=True)
 
     # ingest into part table
-    ib_part = QueryBuffer(alyxraw.AlyxRaw.Field)
+    ib_part = QueryBuffer(alyxraw_dj_module.AlyxRaw.Field)
     for r in tqdm(entries.values()):
         try:
-            field_entry = dict(uuid=r['id'])
+
             for field_name, field_value in r.items():
+                field_entry = dict(uuid=r['id'])
                 if field_name == 'id':
                     continue
                 field_entry['fname'] = field_name
+
+                # in alyxmodel, all foreign key references ends with `_id`,
+                # remove `_id` in the fname to be compatible with previous json dump convention
+                if field_name.endswith('_id'):
+                    field_name = field_name.split('_id')[0]
+
+                # dump the json field
                 if field_name == 'json' and field_value:
                     field_entry['value_idx'] = 0
                     field_entry['fvalue'] = json.dumps(field_value)
@@ -173,10 +207,67 @@ def insert_alyx_entries_model(alyx_model, backtrack_days=None):
         logger.log(25, 'Inserted all remaining raw field tuples')
 
 
+def insert_to_update_alyxraw_postgres(
+        alyx_models=None, delete_update_tables_first=False):
+
+    """Ingest entries into update_ibl_alyxraw from postgres alyx instance
+
+    Args:
+        alyx_models (list of alyx model django objects): list of alyx django models
+        delete_update_tables_first (bool, optional): whether to delete the update module alyx raw tables first. Defaults to False.
+    """
+    if not alyx_models:
+        alyx_models = TABLES_OF_INTEREST
+
+    alyxraw_schema_name = dj.config.get('database.prefix', '') + 'update_ibl_alyxraw'
+
+    with dj.config(safemode=False):
+
+        if delete_update_tables_first:
+            print('Deleting alyxraw update...')
+            # check existence of update_alyxraw
+            if alyxraw_schema_name in dj.list_schemas():
+                alyxraw_update = dj.create_virtual_module(
+                    'alyxraw', alyxraw_schema_name)
+                if hasattr(alyxraw_update, 'AlyxRaw') and alyxraw_update.AlyxRaw:
+                    alyxraw_update.AlyxRaw.Field.delete_quick()
+                    alyxraw_update.AlyxRaw.delete_quick()
+            else:
+                warnings.warn(f'{alyxraw_schema_name} does not exist, create alyxraw_module')
+
+    schema = dj.schema(alyxraw_schema_name)
+
+    @schema
+    class AlyxRaw(dj.Manual):
+        definition = '''
+        uuid: uuid  # pk field (uuid string repr)
+        ---
+        model: varchar(255)  # alyx 'model'
+        '''
+
+        class Field(dj.Part):
+            definition = '''
+            -> master
+            fname: varchar(255)  # field name
+            value_idx: tinyint
+            ---
+            fvalue=null: varchar(40000)  # field value in the position of value_idx
+            index (fname)
+            '''
+    alyxraw_update = dj.create_virtual_module('alyx_raw', alyxraw_schema_name)
+
+    for model in alyx_models:
+        # skip big tables DataSet and FileRecord for updates
+        if model not in [data.models.Dataset, data.models.FileRecord]:
+            logger.log(25, 'Ingesting alyx table {} into datajoint update_alyxraw...'.format(get_alyx_model_name(model)))
+            insert_alyx_entries_model(
+                model, alyxraw_dj_module=alyxraw_update)
+
+
 def main(backtrack_days=3):
 
     for t in TABLES_OF_INTEREST:
-        logger.log(25, 'Ingesting alyx table {} into datajoint alyxraw...'.format(t._meta.db_table))
+        logger.log(25, 'Ingesting alyx table {} into datajoint alyxraw...'.format(get_alyx_model_name(model)))
         insert_alyx_entries_model(t, backtrack_days=backtrack_days)
 
 
