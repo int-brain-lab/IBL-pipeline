a742694470559f2fe5b7e6efc34a0434fefcffae
Thinh Nguyen <thinh@vathes.com>
2021-08-21 13:37:15 -0500

    use dedicated `UpdateAlyxRaw` table

--- ibl_pipeline/ingest/alyxraw.py
+++ ibl_pipeline/ingest/alyxraw.py
@@ -6,6 +6,25 @@ schema = dj.schema(dj.config.get('database.prefix', '') +
 
 
 @schema
+class UpdateAlyxRaw(dj.Manual):
+    definition = '''
+    uuid: uuid  # pk field (uuid string repr)
+    ---
+    model: varchar(255)  # alyx 'model'
+    '''
+
+    class Field(dj.Part):
+        definition = '''
+        -> master
+        fname: varchar(255)  # field name
+        value_idx: tinyint
+        ---
+        fvalue=null: varchar(40000)  # field value in the position of value_idx
+        index (fname)
+        '''
+
+
+@schema
 class AlyxRaw(dj.Manual):
     definition = '''
     uuid: uuid  # pk field (uuid string repr)
