8a1ead88dc8bf2a51ddceea3225e6b82941022c2
Joseph M. Burling <josephburling@gmail.com>
2022-01-11 16:52:41 -0600

    docker image updates

--- docker/iblenv/README.md
+++ docker/iblenv/README.md
@@ -17,18 +17,19 @@ Then from the `IBL-pipeline` directory:
 
 ```bash
 cd docker/iblenv
+VERSION=v1.0.1
+DATE_CREATED=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 docker buildx build \
     --file=Dockerfile \
     --push \
     --platform=linux/amd64,linux/arm64 \
     --target=iblenv_alyx \
-    --tag=iamamutt/iblenv_alyx:v1.0.0 \
+    --tag=iamamutt/iblenv_alyx:$VERSION \
     --tag=iamamutt/iblenv_alyx:latest \
-    --build-arg GROUPNAME=ibl \
-    --build-arg USERNAME=ibluser \
-    --build-arg USER_GID=1000 \
-    --build-arg USER_UID=1000 \
-    --build-arg CONDA_ENV_FILE=iblenv.dj.yml \
+    --build-arg CONDA_ENV_YAML=./iblenv.dj.yml \
+    --build-arg GITHUB_USERNAME=iamamutt \
+    --build-arg IMAGE_CREATED=$DATE_CREATED \
+    --build-arg IMAGE_VERSION=$VERSION \
     .
 ```
 
@@ -50,11 +51,11 @@ docker build \
     --target=iblenv_alyx \
     --tag=iblenv_alyx:v1.0.0 \
     --tag=iblenv_alyx:latest \
-    --build-arg GROUPNAME=ibl \
-    --build-arg USERNAME=ibluser \
+    --build-arg USER_GROUP=ibl \
+    --build-arg USER_NAME=ibluser \
     --build-arg USER_GID=1000 \
     --build-arg USER_UID=1000 \
-    --build-arg CONDA_ENV_FILE=iblenv.dj.yml \
+    --build-arg CONDA_ENV_YAML=iblenv.dj.yml \
     .
 ```
 
@@ -67,6 +68,6 @@ docker push iamamutt/iblenv_alyx:latest
 ```
 -->
 
-The environment variable `CONDA_ENV_FILE` allows for using a different conda environment file. Leave blank to use the default iblenv.yaml from the iblenv GitHub repo. If the standard iblenv.yaml setup fails, try a different one such as: `docker/iblenv/iblenv.dj.yml`. The path should be relative to the build context.
+The environment variable `CONDA_ENV_YAML` allows for using a different conda environment file. Leave blank to use the default iblenv.yaml from the iblenv GitHub repo. If the standard iblenv.yaml setup fails, try a different one such as: `docker/iblenv/iblenv.dj.yml`. The path should be relative to the build context.
 
 Change `--platform` to a valid architecture name to use another platform when building, e.g., `linux/aarch64`.
--- docker/iblenv/test_init.py
+++ docker/iblenv/test_init.py
@@ -1,8 +1,8 @@
 import os
+import sys
 import tempfile
-import json
-from shutil import copyfile, rmtree
 from pathlib import Path
+from shutil import copyfile, rmtree
 
 
 def load_env(file):
@@ -27,7 +27,7 @@ def make_dirs(ibl_path_root=None, remove=False, host="private"):
 
     if remove:
         rmtree(ibl_path_root, ignore_errors=True)
-        rmtree("~/.one", ignore_errors=True)
+        rmtree(resolve_path("~/.one"), ignore_errors=True)
 
     ibl_path_data = ibl_path_root / "data"
     ibl_path_alyx = ibl_path_data / "alyx" / "cache" / host
@@ -44,17 +44,21 @@ def make_dirs(ibl_path_root=None, remove=False, host="private"):
     }
 
 
-def env_test():
+def env_test(
+    ibl_path_root="~/Datasets/int-brain-lab", env_file="", host="private", remove=False
+):
     this_dir = Path(__file__).parent
 
     # load environment variables from .env file
-    env_file = this_dir / ".." / "ibldatajoint" / ".env"
+    if not env_file:
+        env_file = this_dir / ".." / "ibldatajoint" / ".env"
+    env_file = resolve_path(env_file)
     if not env_file.exists():
         raise FileNotFoundError("wrong path for .env file")
     load_env(env_file.resolve())
 
     # make folder structure
-    dirs = make_dirs("~/Datasets/int-brain-lab", True, "private")
+    dirs = make_dirs(ibl_path_root, remove, host)
     os.environ["IBL_PATH_ROOT"] = dirs["root"].as_posix()
     os.environ["IBL_PATH_DATA"] = dirs["data"].as_posix()
     os.environ["IBL_PATH_SHARED"] = dirs["shared"].as_posix()
@@ -70,22 +74,27 @@ def env_test():
     local_one_params = this_dir / ".." / ".." / "shared" / "local.one_params"
     copyfile(local_one_params, tmp_local)
 
+    if not this_dir.as_posix() in sys.path:
+        sys.path.append(this_dir.as_posix())
+
     from config_init import (
-        get_config,
-        init_one_alyx,
-        init_dj_config,
-        one_params_mappings,
-        dj_config_mappings,
-        _dest_one_params_path,
         _dest_dj_config_path,
+        _dest_one_params_path,
+        dj_config_mappings,
+        get_config,
+        init_dj_config,
+        init_one_alyx,
+        one_params_mappings,
     )
 
     params = get_config(tmp_local, "alyx", one_params_mappings(), None)
     config = get_config(tmp_json, "datajoint", dj_config_mappings(), None)
 
-    init_one_alyx("local")
+    init_one_alyx(host)
     _dest_one_params_path.unlink(True)
 
     init_dj_config(tmp_json)
     copyfile(_dest_dj_config_path, dirs["root"] / "dj_local_conf.json")
     _dest_dj_config_path.unlink(True)
+
+    return dirs
