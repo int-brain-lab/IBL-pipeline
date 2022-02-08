f2b2b1dcaa36ced8ee8de059a77c2b5d95c1024e
Thinh Nguyen <thinh@vathes.com>
2022-02-08 10:34:14 -0600

    bugfix IncompleteTrialSession

--- ibl_pipeline/behavior_shared.py
+++ ibl_pipeline/behavior_shared.py
@@ -199,7 +199,7 @@ class CompleteTrialSession(dj.Computed):
             self.insert1(key)
         else:
             IncompleteTrialSession.insert1({**key, 'reason': 'missing required files'}, allow_direct_insert=True)
-            IncompleteTrialSession.insert([{**key, 'missing_file': f} for f in missing_files], allow_direct_insert=True)
+            IncompleteTrialSession.MissingFile.insert([{**key, 'missing_file': f} for f in missing_files], allow_direct_insert=True)
 
 
 @schema
